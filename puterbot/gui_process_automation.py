8cc0a660fabcf2c1b68a472e09e27d80b362c317
Avid <avideslami@gmail.com>
2023-04-19 14:13:11 -0400

    parse output into JSON format

--- puterbot/gui_process_automation.py
+++ puterbot/gui_process_automation.py
@@ -7,7 +7,7 @@ from puterbot.crud import get_screenshots
 import json
 import numpy as np
 import cv2
-
+import re
 import tempfile
 from PIL import Image
 
@@ -26,6 +26,23 @@ model = GPT2LMHeadModel.from_pretrained("gpt2")
 # Initialize PaddleOCR model
 ocr = PaddleOCR(lang='en') #specify languaget to ease processing
 
+
+
+
+def parse_output(output_text):
+    regex = r"\[\[([\d., ]+)\], \[\"(\w+)\", ([\d.]+)\]\]"
+    matches = re.findall(regex, output_text)
+    elements = []
+
+    for match in matches:
+        coordinates = [list(map(float, pair.split(", "))) for pair in match[0].split("], [")]
+        label = match[1]
+        confidence = float(match[2])
+        elements.append({"coordinates": coordinates, "label": label, "confidence": confidence})
+
+    return {"elements": elements}
+
+
 def save_temp_image(image):
     temp_file = tempfile.NamedTemporaryFile(delete=False, suffix='.png')
     temp_file_path = temp_file.name
@@ -34,6 +51,26 @@ def save_temp_image(image):
 
     return temp_file_path
 
+
+def generate_json_output(prompt, max_attempts=5):
+    for attempt in range(max_attempts):
+        input_ids = tokenizer.encode(prompt, return_tensors="pt", max_length=1024, truncation=True)
+        output = model.generate(input_ids, max_length=200, num_return_sequences=1)
+        output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+        
+        try:
+            return parse_output(output_text)
+        except Exception as e:
+            print(f"Attempt {attempt + 1}: Failed to parse output: {output_text}")
+            print(f"Error: {e}")
+            if attempt == max_attempts - 1:
+                raise
+            else:
+                continue
+
+
+
+
 def generate_input_event(new_screenshot, recording):
 
     # load all relevant variables
@@ -41,13 +78,18 @@ def generate_input_event(new_screenshot, recording):
     prevImage = get_screenshots(recording, True)[0].image
 
     # create temporary image paths
-    newImage = save_temp_image(newImage)
-    prevImage = save_temp_image(prevImage)
+    newImagePath = save_temp_image(newImage)
+    prevImagePath = save_temp_image(prevImage)
 
-    newImageText = ocr.ocr(newImage)
-    prevImageText = ocr.ocr(prevImage)
+    newImageText = ocr.ocr(newImagePath)
+    prevImageText = ocr.ocr(prevImagePath)
+    print("New Image Text: ", newImageText)
+    print("Previous Image Text: ", prevImageText)
 
     
+    newImage = np.array(newImage)
+    prevImage = np.array(prevImage)
+
     #InputEventList = recording.screenshots
 
     # Use the Segment Anything library to segment the objects in the new and previous screenshots.
@@ -59,8 +101,8 @@ def generate_input_event(new_screenshot, recording):
     
 
 
-    recording = sam_predictor.predict(recording)
-    new_screenshot = sam_predictor.predict(new_screenshot)
+    #recording = sam_predictor.predict(recording)
+    #new_screenshot = sam_predictor.predict(new_screenshot)
     # Use the PaddleOCR library to extract text from the new and previous screenshots.
 
     newImageText = json.dumps(newImageText)
@@ -70,14 +112,19 @@ def generate_input_event(new_screenshot, recording):
     prompt = f"New Screenshot: {newImageText}, Previous Screenshot: {prevImageText}, Predict the next input event: "
 
     # Tokenize the prompt
-    input_ids = tokenizer.encode(prompt, return_tensors="pt")
+    # input_ids = tokenizer.encode(prompt, return_tensors="pt")
+    input_ids = tokenizer.encode(prompt, return_tensors="pt", max_length=1024, truncation=True)
 
-    # Use the GPT-J library to generate the next input event.
-    output = model.generate(input_ids, max_length=100, num_return_sequences=1)
-    output_text = tokenizer.decode(output[0], skip_special_tokens=True)
 
-    # Parse the output text to extract the predicted InputEvent properties
-    predicted_properties = json.loads(output_text)
+    # # Use the GPT-J library to generate the next input event.
+    # output = model.generate(input_ids, max_length=100, num_return_sequences=1)
+    # output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    # # Parse the output text to extract the predicted InputEvent properties
+    # predicted_properties = json.loads(output_text)
+
+    predicted_properties = generate_json_output(prompt)
+
 
     # Create a new InputEvent object based on the predicted properties
     new_input_event = InputEvent(**predicted_properties)
