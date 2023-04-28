c38edd0a548c5088c6bd951efad944629f669ad3
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2023-04-27 22:24:30 -0400

    Fixed compiling issues with guardrail integration and replaced rail_spec with another example

--- puterbot/strategies/demo.py
+++ puterbot/strategies/demo.py
@@ -9,6 +9,7 @@ Usage:
 from loguru import logger
 import numpy as np
 import guardrails as gd
+import json
 
 from puterbot.events import get_events
 from puterbot.models import Recording, Screenshot
@@ -20,8 +21,34 @@ from puterbot.strategies.llm_mixin import (
 from puterbot.strategies.ocr_mixin import OCRReplayStrategyMixin
 from puterbot.strategies.ascii_mixin import ASCIIReplayStrategyMixin
 
-guard_rail_path = "rail_spec.rail"
-guard = gd.Guard.from_rail(guard_rail_path)
+rail_str = """
+<rail version="0.1">
+
+<output>
+    <choice name="action" on-fail-choice="reask">
+        <case name="fight">
+            <string name="weapon" format="valid-choices: {['crossbow', 'machine gun']}" on-fail-valid-choices="reask" />
+        </case>
+        <case name="flight">
+            <object name="flight">
+                <string name="flight_direction" format="valid-choices: {['north','south','east','west']}" on-fail-valid-choices="exception" />
+                <integer name="distance" format="valid-choices: {[1,2,3,4]}" on-fail-valid-choices="exception" />
+            </object>
+        </case>
+    </choice>
+</output>
+
+<prompt>
+You are a human in an enchanted forest. You come across opponents of different types, and you should fight smaller opponents and run away from bigger ones.
+
+You run into a gang member. What do you do?
+
+@complete_json_suffix_v2</prompt>
+
+</rail>
+"""
+
+guard = gd.Guard.from_rail_string(rail_str)
 
 
 class DemoReplayStrategy(
@@ -38,10 +65,27 @@ class DemoReplayStrategy(
         super().__init__(recording)
         self.result_history = []
 
+    def my_llm_api(self, prompt: str, **kwargs) -> str:
+        """Custom LLM API wrapper.
+
+        Args:
+            prompt (str): The prompt to be passed to the LLM API
+            **kwargs: Any additional arguments to be passed to the LLM API
+
+        Returns:
+            str: The output of the LLM API
+        """
+        max_tokens = kwargs.get('max_tokens', 10)
+        # Call your LLM API here
+        completion = self.get_completion(prompt, max_tokens)
+        logger.info(f"{completion=}")
+        return completion
+
     def get_next_input_event(
         self,
         screenshot: Screenshot,
     ):
+        print("HELLO")
         ascii_text = self.get_ascii_text(screenshot)
         #logger.info(f"ascii_text=\n{ascii_text}")
 
@@ -61,27 +105,30 @@ class DemoReplayStrategy(
         prompt = prompt[N:]
         logger.info(f"{prompt=}")
         max_tokens = 10
-        completion = self.get_completion(prompt, max_tokens)
-        logger.info(f"{completion=}")
 
         # Wrap the get_completion() method with Guard object
         validated_output = guard(
-            self.get_completion,
+            self.my_llm_api,
             prompt_params={"prompt": prompt},
             engine="text-davinci-003",
             max_tokens=max_tokens,
         )
         logger.info(f"{validated_output=}")
 
+        #event_dict = json.loads(validated_output)
+        # input_event = InputEvent(**event_dict)
+        # play_input_event(input_event)
+        print(validated_output)
+
+        result = ""
         try:
-            exec(validated_output["bank_run"])
+            exec(validated_output["action"])
             print("Success! Valid Data")
-            result = validated_output["bank_run"]["explanation"]
+            result = validated_output["action"]
 
         except Exception as e:
             # if there are exceptions use default result from get_completion
-            result = completion.split(">")[0].strip(" <>")
-            print("Failed!")
+            print(e)
 
         logger.info(f"{result=}")
         self.result_history.append(result)
