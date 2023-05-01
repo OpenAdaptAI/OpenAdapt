39d03007da38c454ab236697be4f9e4d86f611af
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2023-05-01 02:28:55 -0400

    Modified prompt to include ocr text and recording description, created custom parser for llm output, and play input events

--- puterbot/strategies/demo.py
+++ puterbot/strategies/demo.py
@@ -7,12 +7,18 @@ Usage:
 """
 
 from loguru import logger
+from typing import List, Dict, Union
+from pynput import keyboard, mouse
+
+import pynput.keyboard
 import numpy as np
 import guardrails as gd
 import json
+import re
 
 from puterbot.events import get_events
-from puterbot.models import Recording, Screenshot
+from puterbot.playback import play_input_event
+from puterbot.models import Recording, Screenshot, InputEvent
 from puterbot.strategies.base import BaseReplayStrategy
 from puterbot.strategies.llm_mixin import (
     LLMReplayStrategyMixin,
@@ -25,7 +31,7 @@ rail_str = """
 <rail version="0.1">
 
 <output>
-    <object name="">
+    <object name="dragon">
         <string name="name" format="valid-choices: {['move', 'click', 'scroll', 'doubleclick', 'singleclick', 'press', 'release', 'type']}" on-fail-valid-choices="reask" />
         <integer name="x" description="The x coordinate of the InputEvent" />
         <integer name="y" description="The y coordinate of the InputEvent" />
@@ -37,18 +43,14 @@ rail_str = """
     </object>
 </output>
 
-<instructions>
-You are a helpful assistant only capable of communicating with valid JSON, and no other text.
-
-@json_suffix_prompt_examples
-</instructions>
-
 <prompt>
 Extract information from this document and return a JSON that follows the correct schema.
 
 @xml_prefix_prompt
 
-{{prompt}}
+{output_schema}
+
+@json_suffix_prompt_v2_wo_none
 
 </prompt>
 
@@ -71,8 +73,9 @@ class DemoReplayStrategy(
     ):
         super().__init__(recording)
         self.result_history = []
+        self.prompt = ""
 
-    def my_llm_api(self, prompt: str, **kwargs) -> str:
+    def my_llm_api(self, prompt, **kwargs) -> str:
         """Custom LLM API wrapper.
 
         Args:
@@ -82,17 +85,88 @@ class DemoReplayStrategy(
         Returns:
             str: The output of the LLM API
         """
-        max_tokens = kwargs.get('max_tokens', 10)
+        max_tokens = 100
         # Call your LLM API here
         completion = self.get_completion(prompt, max_tokens)
         logger.info(f"{completion=}")
         return completion
+   
+    def parse_input_event(self, completion):
+        # Define a list of allowed input event names
+        allowed_names = ['move', 'click', 'scroll', 'doubleclick', 'singleclick', 'press', 'release', 'type']
+        # Split the completion string by the first occurrence of the "[" character and remove unnecessary characters
+        completion = completion.split('[')
+        s = completion[1].replace(", ", "").replace(",", "").replace("'", "")
+        # Extract the command string from the modified completion string
+        command_string = [x.strip() for x in s.split("'") if x.strip()]
+        # Split the command string by the allowed input event names
+        commands = re.split('(move|click|scroll|doubleclick|singleclick|press|release|type)', command_string[0])
+        # Remove empty items from the commands list
+        commands = list(filter(None, commands))
+        # Initialize variables
+        result = []
+        current = ''
 
+        # Iterate over the commands list and create input events based on the allowed input event names
+        for item in commands:
+            if item in allowed_names:
+                if current:
+                    result.append(current)
+                    current = ''
+                result.append(item)
+            else:
+                current += item
+        if current:
+            result.append(current)
+
+        # Merge consecutive input events that belong to the same action
+        merged_commands = [result[i] + result[i+1] for i in range(0, len(result), 2)]
+
+        # Iterate over the merged input events and create a list of input event dictionaries
+        res = []
+        for command in merged_commands:
+            input_dict = {
+                "name": "",
+                "mouse_x": 0,
+                "mouse_y": 0,
+                "mouse_dx": 0,
+                "mouse_dy": 0,
+                "mouse_button_name": "left",
+                "mouse_pressed": False,
+                "canonical_key_char": ""
+            }
+            # Split the input event string into a list of arguments
+            args = command.split()
+            name = args[0]
+            input_dict["name"] = name
+            if name == "press":
+                input_dict["mouse_pressed"] = True 
+            else: 
+                input_dict["mouse_pressed"] = False
+            if name == "move":
+                input_dict["mouse_x"] = args[1]
+                if len(args) > 2:
+                    input_dict["mouse_y"] = args[2]
+            elif name in ["click", "doubleclick", "singleclick", "press", "release"]:
+                # Make sure the key is in valid Canonical form
+                input_dict["canonical_key_char"] = (args[1])
+            elif name == "scroll":
+                input_dict["mouse_dx"] = args[1]
+                if len(args) > 2:
+                    input_dict["mouse_dy"] = args[2]
+            elif name == "type":
+                input_dict["canonical_key_char"] = (args[1])
+            res.append(input_dict)
+
+        # Return the list of input event dictionaries
+        return res
+
+    
     def get_next_input_event(
         self,
         screenshot: Screenshot,
     ):
-        print("HELLO")
+
         ascii_text = self.get_ascii_text(screenshot)
         #logger.info(f"ascii_text=\n{ascii_text}")
 
@@ -100,46 +174,51 @@ class DemoReplayStrategy(
         #logger.info(f"ocr_text=\n{ocr_text}")
 
         event_strs = [
-            f"<{event}>"
+            f"{event}, "
             for event in self.recording.input_events
         ]
         history_strs = [
             f"<{completion}>"
             for completion in self.result_history
         ]
-        prompt = " ".join(event_strs + history_strs)
-        N = max(0, len(prompt) - MAX_INPUT_SIZE)
-        prompt = prompt[N:]
-        logger.info(f"{prompt=}")
-        max_tokens = 10
+
+        self.prompt = "Generate the next input event based on the following:\n\n" \
+                 "Task goal: {}\n\n" \
+                 "Previously recorded input events: {}\n\n" \
+                 "Using the previously recorded input events, generate a sequence of input events to complete the task as a list: " \
+            .format(self.recording.task_description,
+                    event_strs, ocr_text)
+        logger.info(f"{self.prompt=}")
+
+        max_tokens = 100
+        completion = self.get_completion(self.prompt, max_tokens)
+        logger.info(f"{completion=}")
+
+        parsed_events = self.parse_input_event(completion)
+        logger.info(f"{parsed_events=}")
+
 
         # Wrap the get_completion() method with Guard object
-        validated_output = guard(
-            self.my_llm_api,
-            prompt_params={"prompt": prompt},
-            engine="text-davinci-003",
-            max_tokens=max_tokens,
-        )
-        logger.info(f"{validated_output=}")
+        # validated_output = guard(
+        #     self.my_llm_api,
+        #     prompt_params={"prompt": self.prompt},
+        #     engine="text-davinci-003",
+        #     max_tokens=max_tokens,
+        # )
+        # logger.info(f"{validated_output=}")
 
-        #event_dict = json.loads(validated_output)
-        # input_event = InputEvent(**event_dict)
-        # play_input_event(input_event)
-        print(validated_output)
-
-        result = ""
-        try:
-            exec(validated_output["action"])
-            print("Success! Valid Data")
-            result = validated_output["action"]
-
-        except Exception as e:
-            # if there are exceptions use default result from get_completion
-            print(e)
-
-        logger.info(f"{result=}")
-        self.result_history.append(result)
-
-        # TODO: parse result into InputEvent(s)
+        keyboard_controller = keyboard.Controller()
+        mouse_controller = mouse.Controller()
+        # play all the parsed InputEvents
+        for event in parsed_events:
+            try:
+                input_event = InputEvent(**event)
+                play_input_event(
+                    input_event,
+                    mouse_controller,
+                    keyboard_controller)
+            except Exception as e:
+                # if there are errors playing input events
+                print(e)
 
         return None
