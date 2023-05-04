d713fddd8d52d0e0f4d6879698484c7bf6b846bb
AvidEslami <avideslami@gmail.com>
2023-05-04 18:53:26 -0400

    test using action prompts and increasing token limit

---
+++ puterbot/strategies/demo.md
@@ -0,0 +1,1 @@
+How can we use OCR and ASCII data to create InputEvents effectively?
--- puterbot/strategies/demo.py
+++ puterbot/strategies/demo.py
@@ -32,12 +32,16 @@ class DemoReplayStrategy(
         recording: Recording,
     ):
         super().__init__(recording)
+        self.input_event_idx = -1
         self.result_history = []
 
     def get_next_input_event(
         self,
         screenshot: Screenshot,
     ):
+        
+        self.input_event_idx += 1
+        print("Input Event Index: ",self.input_event_idx) #Test Print
         ascii_text = self.get_ascii_text(screenshot)
         #logger.info(f"ascii_text=\n{ascii_text}")
 
@@ -48,23 +52,37 @@ class DemoReplayStrategy(
             f"<{event}>"
             for event in self.recording.input_events
         ]
+        print(event_strs) #Test Print
         history_strs = [
             f"<{completion}>"
             for completion in self.result_history
         ]
+        print(history_strs) #Test Print
+
         prompt = " ".join(event_strs + history_strs)
         N = max(0, len(prompt) - MAX_INPUT_SIZE)
-        prompt = prompt[N:]
+        prompt = prompt[N:]          #Ensure prompt is not too long
         logger.info(f"{prompt=}")
-        max_tokens = 10
-        completion = self.get_completion(prompt, max_tokens)
+        max_tokens = 20
+        action_prompt = prompt.split(">")[self.input_event_idx] + ">"
+        print("Action Prompt: ",action_prompt) #Test Print
+        #completion = self.get_completion(prompt, max_tokens)
+        #Try using action_prompt instead of prompt to generate completions
+        completion = self.get_completion(action_prompt, max_tokens)
         logger.info(f"{completion=}")
 
-        # only take the first <...>
+        # only take the first <...>                            <1><2><3><4>   -->  [<1>,<2>,<3>,<4>] --> <1> --> 1
         result = completion.split(">")[0].strip(" <>")
         logger.info(f"{result=}")
         self.result_history.append(result)
 
         # TODO: parse result into InputEvent(s)
+        # In the naive implementation, we return the recorded InputEvent
+        # In the demo implementation, we would like to return a more sophisticated InputEvent
+
+        # If there is a windowEvent, find an ASCII image of the window
+
+
+
 
         return None
