cdaee2a01dfe0325a246d17b2bf9f3d90a1d487d
Aaron <57018940+0dm@users.noreply.github.com>
2023-05-06 20:51:08 -0400

    wip fastapi
    fastapi on localhost

---
+++ puterbot/plugin/data/demo.py
@@ -0,0 +1,40 @@
+from pynput.keyboard import Key, Controller
+from dotenv import load_dotenv
+import os
+import openai
+
+load_dotenv()
+
+openai.api_key = os.getenv("API_KEY")
+
+params = {
+  'engine': 'text-davinci-003', 
+  'prompt': '', 
+  'max_tokens': 496, 
+  'temperature': 0.7, 
+}
+
+endpoint = "localhost:3333"
+
+keyboard = Controller()
+
+def sendkeys(args):
+    # open sublime in new terminal window
+    os.system("open /Applications/Sublime\ Text.app")
+
+    params.update({"prompt": args})
+    response = openai.Completion.create(**params)
+
+    print("Sending keys: ", response["choices"][0]["text"])
+    # sleep for 3 seconds
+    # __import__("time").sleep(3)
+    keyboard.type(response["choices"][0]["text"])
+    return 200
+
+def test():
+    a = sendkeys("Hello, world!")
+    if a == 200:
+        print("Success!")
+
+if __name__ == "__main__":
+    print(os.getenv("API_KEY"))
