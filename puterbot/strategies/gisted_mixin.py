24cc26c52060298337b4e11d5ebad1a86d8c625f
FFFiend <owais.zahid@mail.utoronto.ca>
2023-05-09 22:45:06 -0400

    Added skeleton GistedLLMReplayStrat

---
+++ puterbot/strategies/gisted_mixin.py
@@ -0,0 +1,49 @@
+"""
+Implements an ReplayStrategy mixin for generating LLM completions
+augmented with gisting on inputs.
+"""
+
+from loguru import logger
+import transformers as tf
+
+from puterbot.models import Recording
+from puterbot.strategies.base import BaseReplayStrategy
+
+from gisting.src import compress
+
+MAX_INPUT_SIZE = 1024
+# 
+MODEL_NAME = "t5"
+
+class GistedLLMReplayStrategyMixin(BaseReplayStrategy):
+    
+    def __init__(self, recording: Recording, model_name: str = MODEL_NAME, max_input_size: int = MAX_INPUT_SIZE):
+        super.__init__(Recording)
+
+        logger.info(f"{model_name=}")
+        self.max_input_size = max_input_size
+
+
+    def get_completion(self, prompt: str, max_tokens: int):
+
+        max_input_size = self.max_input_size
+        if max_input_size and len(prompt) > max_input_size:
+            logger.warning(
+                f"Truncating from {len(prompt)=} to {max_input_size=}"
+            )
+            prompt = prompt[-max_input_size:]
+            logger.warning(
+                f"Truncated {len(prompt)=}"
+            )
+        
+        logger.debug(f"{prompt=} {max_tokens=}")
+
+        gisted_output = compress.compress(MODEL_NAME, prompt)
+        logger.debug(f"{output=}")
+        return gisted_output
+
+
+
+
+
+
