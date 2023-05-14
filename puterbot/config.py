52251ddc5a51f25af887f18110312a6d25f1a0ff
Jamie Tremaine <jamie.tremaine@gmail.com>
2023-05-14 03:00:17 -0400

    Added OpenAI support to LLM Mixin
    The system defaults to gpt-3.5-turbo on OpenAI.
    The original Hugging Face support is still available.
    The LLM mixin now takes an additional platform variable.
    The use of OpenAI as a platform requires:
     - The OPENAI_SYSTEM_MESSAGE be set in config.py
     - The OpenAI API key be included in keys.json in the root folder
    
    Note that the keys.json file should have the following format:
    '{ "openai": "[key]" }'

--- puterbot/config.py
+++ puterbot/config.py
@@ -1,3 +1,4 @@
+import json
 import pathlib
 
 ROOT_DIRPATH = pathlib.Path(__file__).parent.parent.resolve()
@@ -6,3 +7,29 @@ DB_FNAME = "puterbot.db"
 DB_FPATH = ROOT_DIRPATH / DB_FNAME
 DB_URL = f"sqlite:///{DB_FPATH}"
 DB_ECHO = False
+
+# Load the OpenAI key from keys.json
+# Note that the file should be formatted as so: '{ "openai": "[key]" }'
+KEYS_FNAME = "keys.json"
+KEYS_FPATH = ROOT_DIRPATH / KEYS_FNAME
+with open(KEYS_FPATH) as key_file:
+	key_text = key_file.read()
+	key_data = json.loads(key_text)
+
+OPENAI_APIKEY = ""
+if "openai" in key_data:
+    OPENAI_APIKEY = key_data['openai']
+
+OPENAI_SYSTEM_MESSAGE = '''The user will prompt you with a list of commands in angular brackets separated by spaces.
+Each command has a keyword followed by parameters as so: <keyword parameter1 parameter2 parameter3>.
+There can be one or more parameters.
+
+Note that the first or the last command may be truncated. Discard any truncated commands.
+Eliminate commands that have same keyword as the previous command.
+
+An example command sequence could be:
+201 304> <move 202 304> <click 1036 696 left True> <click 1036 696 left> <move 203 304> <move 205 305> <press Key.ctrl_a> <press Key.ctrl_a> <press Key.ctrl_a> <press
+
+For the example above, the correct answer would be:
+<move 202 304> <click 1036 696 left True> <move 203 304> <press Key.ctrl_a>
+'''
--- puterbot/strategies/llm_mixin.py
+++ puterbot/strategies/llm_mixin.py
@@ -7,32 +7,47 @@ Usage:
         ...
 """
 
-
 from loguru import logger
 import transformers as tf  # RIP TensorFlow
+import openai
 
+import puterbot.config as config
 from puterbot.models import Recording
 from puterbot.strategies.base import BaseReplayStrategy
 
+# Model options:
+# hugging_face: gpt2, gpt2-xl is bigger and slower
+# openai: gpt-3.5-turbo, gpt-4, gpt-4-32k
+MODEL_NAME = "gpt-3.5-turbo"
 
-MODEL_NAME = "gpt2"  # gpt2-xl is bigger and slower
+# Platform options: 
+# huggingface or openai
+# Note that OPENAI_APIKEY and OPENAI_SYSTEM_MESSAGE must be set in config.py for OpenAI platform
+PLATFORM_NAME = "openai"
+
+# general input parsing
 MAX_INPUT_SIZE = 1024
 
-
 class LLMReplayStrategyMixin(BaseReplayStrategy):
-
     def __init__(
         self,
         recording: Recording,
         model_name: str = MODEL_NAME,
+        platform: str = PLATFORM_NAME,
         max_input_size: str = MAX_INPUT_SIZE,
     ):
         super().__init__(recording)
 
         logger.info(f"{model_name=}")
-        self.tokenizer = tf.AutoTokenizer.from_pretrained(model_name)
-        self.model = tf.AutoModelForCausalLM.from_pretrained(model_name)
+        self.model_name = model_name
+        self.platform = platform
         self.max_input_size = max_input_size
+
+        if self.platform == "huggingface":
+            self.tokenizer = tf.AutoTokenizer.from_pretrained(self.model_name)
+            self.model = tf.AutoModelForCausalLM.from_pretrained(self.model_name)
+        else:
+            openai.api_key = config.OPENAI_APIKEY
 
     def get_completion(
         self,
@@ -50,20 +65,43 @@ class LLMReplayStrategyMixin(BaseReplayStrategy):
             )
 
         logger.debug(f"{prompt=} {max_tokens=}")
-        input_tokens = self.tokenizer(prompt, return_tensors="pt")
-        pad_token_id = self.tokenizer.eos_token_id
-        attention_mask = input_tokens["attention_mask"]
-        output_tokens = self.model.generate(
-            input_ids=input_tokens["input_ids"],
-            attention_mask=attention_mask,
-            max_length=input_tokens["input_ids"].shape[-1] + max_tokens,
-            pad_token_id=pad_token_id,
-            num_return_sequences=1
-        )
-        N = input_tokens["input_ids"].shape[-1]
-        completion = self.tokenizer.decode(
-            output_tokens[:, N:][0],
-            clean_up_tokenization_spaces=True,
-        )
+
+        if self.platform == "huggingface":
+            # Hugging face platform
+            input_tokens = self.tokenizer(prompt, return_tensors="pt")
+            pad_token_id = self.tokenizer.eos_token_id
+            attention_mask = input_tokens["attention_mask"]
+            output_tokens = self.model.generate(
+                input_ids=input_tokens["input_ids"],
+                attention_mask=attention_mask,
+                max_length=input_tokens["input_ids"].shape[-1] + max_tokens,
+                pad_token_id=pad_token_id,
+                num_return_sequences=1
+            )
+            N = input_tokens["input_ids"].shape[-1]
+            completion = self.tokenizer.decode(
+                output_tokens[:, N:][0],
+                clean_up_tokenization_spaces=True,
+            )
+            logger.debug(f"{completion=}")
+        else:
+            # Default to OpenAI as the platform
+
+            # Build the messages first.
+            messages = []
+            sys_msg = config.OPENAI_SYSTEM_MESSAGE
+            if sys_msg:
+                messages.append({"role": "system", "content": sys_msg})
+            messages.append({"role":"user", "content": prompt})
+
+            # Get an OpenAI Chat completion
+            response = openai.ChatCompletion.create(
+                model=self.model_name,
+                temperature=0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0,
+                messages=messages
+            )
+            completion = response.to_dict_recursive()['choices'][0]['message']['content']
         logger.debug(f"{completion=}")
         return completion
