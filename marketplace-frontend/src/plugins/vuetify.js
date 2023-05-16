ab4ea4637eb25ccf631cb68b336cb40cf5acc2c3
Jesica <jesica.susanto@mail.utoronto.ca>
2023-05-16 19:21:18 +0700

    add background color to App.vue

--- marketplace-frontend/src/plugins/vuetify.js
+++ marketplace-frontend/src/plugins/vuetify.js
@@ -1,10 +1,21 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+//import '@fortawesome/fontawesome-free/css/all.css'
 
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify(
+export default createVuetify({
   // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+  icons:{
+    iconfont: 'md' || 'fa',
+  },
+  theme:{
+    themes:{
+      dark: {
+        background: '#19244c'
+      }
+    }
+  }
+}
+);
