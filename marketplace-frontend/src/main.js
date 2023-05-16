3cb74a424732017a14f374f5f74ebe94ce4fba42
Jesica <jesica.susanto@mail.utoronto.ca>
2023-05-16 16:05:40 +0700

    add vuetify 3

--- marketplace-frontend/src/main.js
+++ marketplace-frontend/src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
 
-createApp(App).use(store).use(router).mount('#app')
+loadFonts()
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .use(vuetify)
+  .mount('#app')
