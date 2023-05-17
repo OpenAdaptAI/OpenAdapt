8a793a1223a0fc894e294c3a046940bae597f4e2
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-05-17 14:25:18 -0400

    renamed MiniGPT4ReplayStrategy

--- puterbot/strategies/__init__.py
+++ puterbot/strategies/__init__.py
@@ -2,4 +2,4 @@ from puterbot.strategies.base import BaseReplayStrategy
 from puterbot.strategies.naive import NaiveReplayStrategy
 from puterbot.strategies.demo import DemoReplayStrategy
 # add more strategies here
-from puterbot.strategies.minigpt4 import GPTReplayStrategy
+from puterbot.strategies.minigpt4 import MiniGPT4ReplayStrategy
