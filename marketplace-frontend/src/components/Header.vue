c11e41d834b265983775eb979b6d1a5ad77dadbf
Jesica <jesica.susanto@mail.utoronto.ca>
2023-05-18 11:53:14 +0700

    Added empty contact and marketplace view

--- marketplace-frontend/src/components/Header.vue
+++ marketplace-frontend/src/components/Header.vue
@@ -4,8 +4,8 @@
             <v-toolbar-title class="text-h6" >
                 OpenAdapt.AI
             </v-toolbar-title>
-            <v-btn class="mx-2 justify-end">Marketplace</v-btn>
-            <v-btn class="mx-2 justify-end">About</v-btn>
+            <v-btn class="mx-2 justify-end" to = "/">Marketplace</v-btn>
+            <v-btn class="mx-2 justify-end" to ="/about">About</v-btn>
             <v-btn class="mx-2 justify-end">Contact</v-btn>
             <v-spacer></v-spacer>
             <v-btn class="mx-2" variant = "elevated">Log in</v-btn>
