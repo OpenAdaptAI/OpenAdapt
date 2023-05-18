ceaac69ff44f440a9007c26e6c0fe0b9c2c20e49
Krish Patel <kmppatel133@gmail.com>
2023-05-18 11:40:40 -0400

    remove return from utils.py

--- openadapt/utils.py
+++ openadapt/utils.py
@@ -17,7 +17,6 @@ EMPTY = (None, [], {}, "")
 
 
 def configure_logging(logger, log_level):
-    return
     log_level_override = os.getenv("LOG_LEVEL")
     log_level = log_level_override or log_level
     logger.remove()
