e9f69d8944a4270deaa8c63f34d6ae83029f46fb
AvidEslami <avideslami@gmail.com>
2023-05-18 14:14:03 -0400

    fix tests, added file signals

--- openadapt/signals.py
+++ openadapt/signals.py
@@ -29,7 +29,19 @@ class Signals:
         Add a signal from a file.
         """
         # Get the signal from the file.
-        return
+        try:
+            with open(file_path, 'r') as file:
+                content = file.read()
+        except FileNotFoundError:
+            logger.info(f"Error: File not found.")
+            return "Error: File not found."
+        except PermissionError:
+            logger.info(f"Error: Permission denied.")
+            return None
+        except IOError:
+            logger.info(f"Error: I/O error.")
+            return None
+        return content
 
 
     def add_url_signal(self, http_url):
@@ -54,22 +66,25 @@ class Signals:
         return
 
     def add_signal(self, signal_address):
+        signal_data = None
         if isinstance(signal_address, str):
             # If signal is a string, it could be a file path, a database URL,
             # an HTTP URL, or a Python function name.
             if signal_address.startswith("pgsql://"):
                 # If the string starts with "pgsql://", treat it as a database URL.
-                self.add_database_signal(signal_address)
+                signal_data = self.add_database_signal(signal_address)
             elif signal_address.startswith("http://") or signal_address.startswith("https://"):
                 # If the string starts with "http://" or "https://", treat it as an HTTP URL.
-                self.add_url_signal(signal_address)
-            elif signal_address.endswith(('.json', '.csv')):
+                signal_data = self.add_url_signal(signal_address)
+            elif signal_address.endswith(('.json', '.csv', '.txt')):
                 # If the string ends with a known file extension, treat it as a file path.
-                self.add_file_signal(signal_address)
+                signal_data = self.add_file_signal(signal_address)
             else:
                 # Otherwise, treat it as a Python function name.
-                self.add_function_signal(signal_address)
+                signal_data = self.add_function_signal(signal_address)
         else:
             # If signal is not a string, raise an error.
             raise ValueError("Signal must be a string.")
+        
+        self.signals.append(signal_data)
 
--- tests/openadapt/test_signals.py
+++ tests/openadapt/test_signals.py
@@ -1,9 +1,16 @@
 from openadapt.signals import Signals
 
 
+def test_add_file_signal():
+    signals = Signals()
+    signals.add_signal("tests/openadapt/test_signal_data.txt")
+    signal_data = signals.return_signals()
+    assert signal_data == ["test_data_success"]
+
+
 def test_add_url_signal():
     signals = Signals()
-    signals.add_url_signal("https://www.google.com")
+    signals.add_signal("https://www.google.com")
     signal_data = signals.return_signals()
     #Assuming google.com is not down
-    assert signal_data is not None
+    assert signal_data[0] != None
