8a754f842a38bb92888c679eb96789b7da6b2c38
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-05-24 14:31:42 -0400

    check if git repo is downloaded, check ram, only download lfs files once, bug fixes

--- openadapt/autominigpt4.py
+++ openadapt/autominigpt4.py
@@ -7,9 +7,8 @@ Usage: python openadapt/autominigpt4.py
 import subprocess
 import os
 import sys
+import psutil
 
-
-# TODO: assume have Git
 
 def clone_repository(repo_url, target_dir):
     try:
@@ -17,24 +16,60 @@ def clone_repository(repo_url, target_dir):
         print("Repository cloned successfully!")
     except subprocess.CalledProcessError as e:
         print("Failed to clone repository:", e)
+        sys.exit()
 
 
-def shallow_clone_repository(repo_url, target_dir):
+def clone_repository_skip_lfs(repo_url, target_dir):
     try:
-        subprocess.check_call(['git', 'clone', '--depth', '1', repo_url, target_dir])
-        print("Repository cloned successfully!")
+        # Clone the repository with a shallow clone and enable sparse checkout
+        subprocess.check_call(
+            ['git', 'clone', '--depth', '1', '--no-checkout', repo_url,
+             target_dir])
+        os.chdir(target_dir)
+        # Enable sparse checkout
+        subprocess.check_call(['git', 'sparse-checkout', 'init', '--cone'])
+        # Set the sparse-checkout pattern to include all files
+        subprocess.check_call(['git', 'sparse-checkout', 'set', '/*'])
+        # Set the GIT_LFS_SKIP_SMUDGE environment variable to skip
+        os.environ['GIT_LFS_SKIP_SMUDGE'] = '1'
+        # Perform the checkout to download the non-LFS files
+        subprocess.check_call(['git', 'checkout'])
+        # Remove the GIT_LFS_SKIP_SMUDGE environment variable
+        del os.environ['GIT_LFS_SKIP_SMUDGE']
+        print("Repository cloned successfully without downloading LFS files!")
     except subprocess.CalledProcessError as e:
         print("Failed to clone repository:", e)
+        sys.exit()
 
 
-def check_and_clone(repo_url, target_dir, shallow):
+def check_and_clone(repository_url, target_directory, lfs):
+    if os.path.exists(target_directory):
+        os.chdir(target_directory)
+        if not is_valid_git_repository(target_directory):
+            print(
+                f"Directory '{target_directory}' exists but is not a valid Git repository. Please "
+                f"check the files in this directory and choose whether to delete the directory. "
+                f"Only restart the installation process if you have deleted or renamed this "
+                f"directory.")
+            sys.exit()
+        else:
+            print(
+                f"Repository '{target_directory}' already exists and is valid. Skipping cloning "
+                f"step.")
+    else:
+        if not lfs:
+            clone_repository(repository_url, target_directory)
+        else:
+            clone_repository_skip_lfs(repository_url, target_directory)
 
 
-
-def is_valid_git_repository():
+def is_valid_git_repository(target_directory):
     try:
-        subprocess.check_output(['git', 'rev-parse', '--is-inside-work-tree'])
-        return True
+        output = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'])
+        git_toplevel = output.decode().strip()
+        git_toplevel = os.path.normpath(git_toplevel)
+        target_directory = os.path.normpath(target_directory)
+        return git_toplevel == target_directory
     except subprocess.CalledProcessError:
         return False
 
@@ -46,34 +81,23 @@ def download_lfs_files(repo_dir):
         print(f"LFS files downloaded to {repo_dir} successfully!")
     except subprocess.CalledProcessError as e:
         print(f"Failed to download LFS files to {repo_dir}:", e)
+        sys.exit()
 
 
-def delete_files(files_to_delete):
-    for file_name in files_to_delete:
-        try:
-            subprocess.check_call(['git', 'rm', file_name])
-            print(f"{file_name} removed successfully!")
-        except subprocess.CalledProcessError as e:
-            print(f"Failed to remove {file_name}:", e)
+def check_ram_info():
+    ram_info = psutil.virtual_memory()
+    total_ram = ram_info.total / (1024 ** 3)  # Convert to GB
+    print("Total RAM: {:.2f} GB".format(total_ram))
+    return total_ram
 
 
 if __name__ == "__main__":
     # clone MiniGPT-4
     minigpt4 = os.path.abspath('MiniGPT4')
     # Check if the repository already exists
-    if os.path.exists(minigpt4):
-        os.chdir(minigpt4)
-        if not is_valid_git_repository():
-            print(f"Directory '{minigpt4}' exists but is not a valid Git repository. Please"
-                  f"check the files in this directory and choose whether to delete the directory."
-                  f"Only restart the installation process if you have deleted or renamed "
-                  f"this directory.")
-            sys.exit()
-        else:
-            print(f"Repository '{minigpt4}' already exists and is valid. Skipping cloning step.")
-    else:
-        clone_repository('https://github.com/Vision-CAIR/MiniGPT-4', minigpt4)
-        os.chdir(minigpt4)
+    check_and_clone('https://github.com/Vision-CAIR/MiniGPT-4', minigpt4, False)
+
+    os.chdir(minigpt4)
 
     # install git lfs
     try:
@@ -82,27 +106,19 @@ if __name__ == "__main__":
     except subprocess.CalledProcessError as e:
         print("Failed to install Git LFS:", e)
 
-    # TODO: test below
-    # clone vicuna 13b v1.1 weights
+    # clone vicuna 13b v1.1 weights without the lfs files
     vicuna = os.path.join(minigpt4, 'vicuna-13b-delta-v1.1')
-    clone_repository('https://huggingface.co/lmsys/vicuna-13b-delta-v1.1', vicuna)
-    # delete all the partially downloaded files
+    check_and_clone('https://huggingface.co/lmsys/vicuna-13b-delta-v1.1', vicuna, True)
     os.chdir(vicuna)
-    delete_files(['pytorch_model-00001-of-00003.bin', 'pytorch_model-00002-of-00003.bin',
-                  'pytorch_model-00003-of-00003.bin'])
-    # redownload the complete large size files
+    # download the complete large size files
     download_lfs_files(vicuna)
     os.chdir(minigpt4)
 
     # clone llama 13b weights
     llama = os.path.join(minigpt4, 'llama-13b')
-    clone_repository('https://huggingface.co/huggyllama/llama-13b', llama)
-    # delete all the partially downloaded files
+    check_and_clone('https://huggingface.co/huggyllama/llama-13b', llama, True)
     os.chdir(llama)
-    delete_files(['pytorch_model-00001-of-00003.bin', 'pytorch_model-00002-of-00003.bin',
-                  'pytorch_model-00003-of-00003.bin', 'model-00001-of-00003.safetensors',
-                  'model-00002-of-00003.safetensors', 'model-00003-of-00003.safetensors'])
-    # redownload the complete large size files
+    # download the complete large size files
     download_lfs_files(llama)
 
     # pip3 install fschat
@@ -113,15 +129,18 @@ if __name__ == "__main__":
     except subprocess.CalledProcessError as e:
         print("Failed to install FastChat:", e)
 
-    # python -m fastchat.model.apply_delta --base-model-path D:\llama-13b
-    # --target-model-path D:\vicuna_weights --delta-path D:\vicuna-13b-delta-v1.1 --low-cpu-mem
     # create working weight
     target = os.path.join(minigpt4, 'vicuna')
-    # assuming most people don't have 60GB of CPU RAM, so we use --low-cpu-mem
+    # First check how much RAM there is. if less than 60GB, we use --low-cpu-mem.
     try:
-        subprocess.check_call(['python', '-m', 'fastchat.model.apply_delta', '--base-model-path',
-                               llama, '--target-model-path', target, '--delta-path', vicuna,
-                               '--low-cpu-mem'])
+        if check_ram_info() < 60:
+            subprocess.check_call(['python', '-m', 'fastchat.model.apply_delta', '--base-model-path',
+                                   llama, '--target-model-path', target, '--delta-path', vicuna,
+                                   '--low-cpu-mem'])
+        else:
+            subprocess.check_call(
+                ['python', '-m', 'fastchat.model.apply_delta', '--base-model-path',
+                 llama, '--target-model-path', target, '--delta-path', vicuna])
         print("Working weight created successfully!")
     except subprocess.CalledProcessError as e:
         print("Failed to create working weight:", e)
