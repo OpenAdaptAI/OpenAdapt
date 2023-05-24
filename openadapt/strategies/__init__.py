9ee00c44351d87770ad898f98296b73ad5c8e2ca
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-05-24 14:34:09 -0400

    removed #

--- openadapt/strategies/__init__.py
+++ openadapt/strategies/__init__.py
@@ -2,4 +2,4 @@ from openadapt.strategies.base import BaseReplayStrategy
 from openadapt.strategies.naive import NaiveReplayStrategy
 from openadapt.strategies.demo import DemoReplayStrategy
 # add more strategies here
-# from openadapt.strategies.minigpt4 import MiniGPT4ReplayStrategy
+from openadapt.strategies.minigpt4 import MiniGPT4ReplayStrategy
