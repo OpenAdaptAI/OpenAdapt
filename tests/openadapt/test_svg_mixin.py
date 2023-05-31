426ac01a5977a70c37ee3441a6ba9184d983b107
Diian-r <dianrong2012@gmail.com>
2023-05-31 16:38:04 -0400

    Initial testing framework

--- tests/openadapt/test_svg_mixin.py
+++ tests/openadapt/test_svg_mixin.py
@@ -1,4 +1,8 @@
-from PIL import Image
+from io import BytesIO
+from PIL import Image, ImageChops
+import cairosvg as cairo
+# NOTE: cairosvg may require additional dependencies
+# as outlined in https://cairosvg.org/documentation/
 
 from openadapt.strategies.demo import DemoReplayStrategy
 from openadapt.models import Recording, Screenshot
@@ -18,10 +22,20 @@ def test_one_button():
     one_button_image = Image.open("assets/one_button.png")
     one_button_screenshot = Screenshot()
     one_button_screenshot._image = one_button_image
-    expected = "(10, 20)"
-    actual = REPLAY.get_svg_info(one_button_screenshot)
-    print(actual)
-    assert actual == expected
+
+    actual_svg_text = REPLAY.get_svg_text(one_button_screenshot)
+
+    expected = one_button_image.convert("L")
+
+    actual_svg_file = BytesIO(actual_svg_text)
+    actual_png_file = BytesIO()
+    cairo.svg2png(file_obj=actual_svg_file, write_to=actual_png_file)
+    actual = Image.open(actual_png_file)
+
+    difference = ImageChops.difference(expected, actual)
+    print(difference)
+    assert difference < 0.2
+
 
 # TODO
 # def test_multiple_buttons():
