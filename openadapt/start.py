8e663645c1942ef49bd2dec11897cff0b7e232ee
Diian-r <dianrong2012@gmail.com>
2023-06-05 17:00:52 -0400

    better checks if changes are needed

--- openadapt/start.py
+++ openadapt/start.py
@@ -2,12 +2,24 @@ import subprocess
 
 from openadapt.app.main import run_app
 
+def changes_needed(result: str) -> bool:
+    """ Checks if git stash needs to be run.
+    
+    Assumes result is the result of git status.
+    """
+    if "Changes to be committed:" in result:
+        return True
+    if "Changes not staged for commit:" in result:
+        return True
+    if "Your branch is ahead of" in result:
+        return True
+    return False
+
 result = subprocess.run(["git", "status"], capture_output=True, text=True)
 
 if "branch is up to date" not in result.stdout:
     
-    if "Changes to be committed:" or \
-        "Changes not staged for commit:" in result.stdout:
+    if changes_needed(result.stdout):
         subprocess.run(["git", "stash"])
         print("Changes stashed")
     
