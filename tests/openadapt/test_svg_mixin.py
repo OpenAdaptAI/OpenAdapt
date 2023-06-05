d1b71b7fe42e9f4374a53a411391bd36f86796fa
Diian-r <dianrong2012@gmail.com>
2023-06-05 16:12:28 -0400

    improve svg test

--- tests/openadapt/test_svg_mixin.py
+++ tests/openadapt/test_svg_mixin.py
@@ -22,19 +22,17 @@ def mse(image1, image2):
 
 
 def test_one_button():
-    # set up the right button
+    # set up the expected image
     one_button_image = Image.open("assets/one_button.png")
     one_button_screenshot = Screenshot()
     one_button_screenshot._image = one_button_image
     expected = one_button_image.convert("L")
 
+    # set up the actual image
     actual_svg_text = REPLAY.get_svg_text(one_button_screenshot)
-
     actual_svg_file = StringIO(actual_svg_text)
-    
     actual_png_file = BytesIO()
     cairo.svg2png(file_obj=actual_svg_file, write_to=actual_png_file)
-
     actual_img = Image.open(actual_png_file)
     actual = actual_img.convert("L")
 
