22e909c8db78fd5ac3e1110f09946635739ba03f
FFFiend <owais.zahid@mail.utoronto.ca>
2023-06-06 18:25:37 -0400

    added elaboration code

--- openadapt/temp_hierarchy_script/gpt_hierarchy.py
+++ openadapt/temp_hierarchy_script/gpt_hierarchy.py
@@ -1,5 +1,5 @@
 import openai
-openai.api_key = "INSERT_API_KEY"
+openai.api_key = "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"
 
 
 completion = openai.ChatCompletion.create(
@@ -26,6 +26,21 @@ tasks_str = completion.choices[0].message['content'].split("NEW")
 tasks_by_level = {}
 for i in range(len(tasks_str)):
     tasks_by_level[i] = tasks_str[i]
+    additional_info = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role":"system",
+        "content":"You are now elaborating on a hierarchy that is part of a \
+            larger hierarchy of actions that \
+                 one takes when operating GUI desktop applications for typical \
+                    day-to-day tasks. Consider different levels of abstractions. \
+                        Examples include: clicking a button, opening a window, \
+                            operating payroll software, generating invoices, \
+                                renting an apartment."},
+                    {"role":"user",
+        "content":"{}".format(tasks_str[i][:3000])}]
+    )
+    
+    tasks_by_level[i] += additional_info.choices[0].message['content']
 
 
 if __name__ == "__main__":
