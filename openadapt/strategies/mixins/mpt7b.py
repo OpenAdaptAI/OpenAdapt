d6410519b9bf52837e04ad77fa73098c9fc336d1
FFFiend <owais.zahid@mail.utoronto.ca>
2023-06-06 04:24:44 -0400

    halfway done with mpt-7b completion

---
+++ openadapt/strategies/mixins/mpt7b.py
@@ -0,0 +1,46 @@
+"""
+Implements a rudimentary mixin for using
+MPT-7B to generate completions.
+
+
+"""
+
+from openadapt.strategies.base import BaseReplayStrategy
+from openadapt.models import Recording
+import transformers
+import modal
+
+stub = modal.Stub()
+
+stub.cls(gpu="t4")
+# We need modal, loading this model takes 12 GB 
+class MPT_7BReplayStrategy(BaseReplayStrategy):
+
+    def __init__(
+        self,
+        max_input_size: int
+    ):
+        #super.__init__(recording)
+
+        self.model = transformers.AutoModelForCausalLM.from_pretrained(
+            'mosaicml/mpt-7b',
+            trust_remote_code=True
+        )
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+            'EleutherAI/gpt-neox-20b')
+
+        self.max_input_size = max_input_size
+
+    def get_completions(self, prompt: str, max_tokens: int):
+        max_input_size = self.max_input_size, 2048
+        if max_input_size > 2048:
+            max_input_size = 2048
+
+        prompt = prompt[-max_input_size:]
+        # sift mpt-7b methods to output the completion
+        # INCOMPLETE
+
+
+
+
+        
