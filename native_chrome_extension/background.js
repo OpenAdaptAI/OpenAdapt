b484f7eccb959872171c708ba03cfec8816f23bc
Krish Patel <kmppatel133@gmail.com>
2023-06-07 10:41:33 -0400

    remove old code

--- native_chrome_extension/background.js
+++ native_chrome_extension/background.js
@@ -1,27 +1,3 @@
-// // Handle received messages
-// function onReceived(response) {
-//     console.log(response);
-// }
-
-// let port = chrome.runtime.connectNative("openadapt");
-// port.onMessage.addListener(onReceived);  // Receive messages from the native messaging host
-// port.postMessage("hello");
-
-// /*
-// On a click on the browser action, send the app a message.
-// */
-// chrome.action.onClicked.addListener((tab) => {
-//   port.postMessage("Thanks! For using OpenAdapt DOM Listener.");
-// });
-
-
-// let observer = new MutationObserver(mutations => {
-//   for(let mutation of mutations) {
-//       chrome.runtime.sendNativeMessage("openadapt", mutation).then(onReceived);
-//   }
-// });
-// observer.observe(document, { childList: true, subtree: true });
-
 
 // Native Messaging port
 let port = null;
@@ -32,9 +8,9 @@ function onReceived(response) {
 }
 
 
-const hostName = 'openadapt'; // Replace with your Native Messaging host name
+const hostName = 'openadapt';
 port = chrome.runtime.connectNative(hostName);
-port.onMessage.addListener(onReceived); // Receive messages from the Native Messaging host
+port.onMessage.addListener(onReceived);
 port.postMessage('hello');
 
 
@@ -61,11 +37,3 @@ function messageListener(message, sender, sendResponse) {
 
 // Listen for messages from content scripts
 chrome.runtime.onMessage.addListener(messageListener);
-
-
-/*
-On a click on the browser action, send a message to the active tab's content script.
-*/
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage(tab.id, { type: 'getDocument' });
-});
