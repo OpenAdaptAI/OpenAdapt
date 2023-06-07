6b327663e51af88341978bd39d250b46303c19b9
AvidEslami <avideslami@gmail.com>
2023-06-07 15:20:49 -0400

    loads parameters

--- openadapt/strategies/mixins/rwkv.py
+++ openadapt/strategies/mixins/rwkv.py
@@ -6,14 +6,16 @@ from openadapt.RWKV.RWKV import run_RWKV
 from openadapt import config
 
 USE_MODAL = config.USE_MODAL
-parameters = config.RWKV_PARAMETERS
+PARAMETERS = config.RWKV_PARAMETERS
 
 class RWKVReplayStrategyMixin:
 
     def get_response(self,instruction=None, task_description=None, input=None, parameters=None):
+        if parameters is None:
+            parameters = PARAMETERS
         if USE_MODAL:
             function_call = modal.Function.lookup("openadapt-rwkv", "run_RWKV")
-            response = function_call.call(task_description=task_description, input=input)
+            response = function_call.call(task_description=task_description, input=input, parameters=parameters)
             logger.debug(f"response=\n{response}")
         else:
             response = run_RWKV(instruction, task_description, input)
