6b53fcd39d5da1c1dfdb8e16c3058537e525b3bb
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-06-07 23:13:27 -0400

    display basic productivity data in html page similar to visualize

--- openadapt/productivity.py
+++ openadapt/productivity.py
@@ -1,3 +1,12 @@
+from pprint import pformat
+from threading import Timer
+import html
+import os
+import string
+
+from bokeh.io import output_file, show
+from bokeh.layouts import layout, row
+from bokeh.models.widgets import Div
 from loguru import logger
 
 from openadapt.crud import (
@@ -9,36 +18,122 @@ from openadapt.events import (
 )
 
 from openadapt.utils import (
-configure_logging
+    configure_logging,
+    display_event,
+    evenly_spaced,
+    image2utf8,
+    EMPTY,
+    row2dict,
+    rows2dicts,
 )
 
-MAX_GAP_SECONDS = 30
-PROCESS_EVENTS = True
+from openadapt.visualize import (
+    CSS,
+    IMG_WIDTH_PCT,
+    MAX_TABLE_CHILDREN,
+    MAX_EVENTS,
+    dict2html
+)
+
+MAX_GAP_SECONDS = 15
+PROCESS_EVENTS = False
 LOG_LEVEL = "INFO"
 
 
 def find_gaps(action_events):
     num_gaps = 0
+    time_in_gaps = 0
     # check every pair of action events for gap length
     for i in range(0, len(action_events) - 1):
-        if action_events[i + 1].screenshot_timestamp - action_events[i].screenshot_timestamp > MAX_GAP_SECONDS:
+        curr_gap = action_events[i + 1].screenshot_timestamp - action_events[i].screenshot_timestamp
+        if curr_gap > MAX_GAP_SECONDS:
             num_gaps += 1
-    return num_gaps
+            time_in_gaps += curr_gap
+    return num_gaps, time_in_gaps
+
+
+def find_clicks(action_events):
+    num_clicks = 0
+    for action_event in action_events:
+        if action_event.mouse_pressed:
+            num_clicks += 1
+    return num_clicks
+
+
+def find_key_presses(action_events):
+    num_key_presses = 0
+    for action_event in action_events:
+        if action_event.name == "press":
+            num_key_presses += 1
+    return num_key_presses
 
 
 def calculate_productivity():
     configure_logging(logger, LOG_LEVEL)
 
     recording = get_latest_recording()
+    logger.debug(f"{recording=}")
 
-    action_events = get_events(recording, process=PROCESS_EVENTS)
+    meta = {}
+    action_events = get_events(recording, process=PROCESS_EVENTS, meta=meta)
+    event_dicts = rows2dicts(action_events)
+    logger.info(f"event_dicts=\n{pformat(event_dicts)}")
 
-    gaps = find_gaps(action_events)
+    # overall info first
+    gaps, time_in_gaps = find_gaps(action_events)
+    num_clicks = find_clicks(action_events)
+    num_key_presses = find_key_presses(action_events)
+    prod_info = {f"Number of pauses longer than {MAX_GAP_SECONDS} seconds": gaps,
+                 "Total time spent during pauses": time_in_gaps,
+                 "Total number of mouse clicks": num_clicks,
+                 "Total number of key presses": num_key_presses
+                 }
+
+    rows = [
+        row(
+            Div(
+                text=f"<style>{CSS}</style>",
+            ),
+        ),
+        row(
+            Div(
+                text=f"{dict2html(row2dict(recording))}",
+            ),
+        ),
+        row(
+            Div(
+                text=f"{dict2html(meta)}",
+                width_policy="max",
+            ),
+        ),
+        row(
+            Div(
+                text=f"{dict2html(prod_info)}"
+            )
+        )
+    ]
     # other productivity metrics
 
-    # display data somehow
-    # for now just printing it
-    logger.info(f"Number of gaps larger than {MAX_GAP_SECONDS} seconds: {gaps}")
+    # window by window
+
+    # display data
+    title = f"Productivity metrics for recording-{recording.id}"
+    fname_out = f"productivity-{recording.id}.html"
+    logger.info(f"{fname_out=}")
+    output_file(fname_out, title=title)
+
+    result = show(
+        layout(
+            rows,
+        )
+    )
+
+    def cleanup():
+        os.remove(fname_out)
+        removed = not os.path.exists(fname_out)
+        logger.info(f"{removed=}")
+
+    Timer(1, cleanup).start()
 
 
 if __name__ == "__main__":
