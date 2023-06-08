8c7c48daa69340e4e51ca9c04fab5e4b550c4bcc
Krish Patel <kmppatel133@gmail.com>
2023-06-08 11:59:59 -0400

    code reformamting in py

--- native_chrome_extension/browser.py
+++ native_chrome_extension/browser.py
@@ -1,46 +1,77 @@
-#!/usr/bin/env -S python3 -u
+""" Module for communicating with the browser extension. """
 
+#!/usr/bin/env -S python3 -u
 # Note that running python with the `-u` flag is required on Windows,
 # in order to ensure that stdin and stdout are opened in binary, rather
 # than text, mode.
+
 
 import sys
 import json
 import struct
 
 
-# Read a message from stdin and decode it.
-def getMessage():
-    rawLength = sys.stdin.buffer.read(4)
-    if len(rawLength) == 0:
+def get_message():
+    """
+    Read a message from stdin and decode it.
+
+    Args:
+        None
+
+    Returns:
+        A Python dictionary representing the message.
+    """
+
+    raw_length = sys.stdin.buffer.read(4)
+    if len(raw_length) == 0:
         sys.exit(0)
-    messageLength = struct.unpack('@I', rawLength)[0]
-    message = sys.stdin.buffer.read(messageLength).decode('utf-8')
+    message_length = struct.unpack("@I", raw_length)[0]
+    message = sys.stdin.buffer.read(message_length).decode("utf-8")
     return json.loads(message)
 
 
-# Encode a message for transmission,
-# given its content.
-def encodeMessage(messageContent):
+def encode_message(message_content):
+    """
+    Encode a message for transmission,
+
+    Args:
+        message_content: The content of the message to be encoded.
+
+    Returns:
+        A Python dictionary representing the encoded message.
+    """
+
     # https://docs.python.org/3/library/json.html#basic-usage
-    # To get the most compact JSON representation, you should specify 
+    # To get the most compact JSON representation, you should specify
     # (',', ':') to eliminate whitespace.
-    # We want the most compact representation because the browser rejects # messages that exceed 1 MB.
-    encodedContent = json.dumps(messageContent, separators=(',', ':')).encode('utf-8')
-    encodedLength = struct.pack('@I', len(encodedContent))
-    return {'length': encodedLength, 'content': encodedContent}
+    # We want the most compact representation because
+    # the browser rejects # messages that exceed 1 MB.
+    encoded_content = json.dumps(
+        message_content, separators=(",", ":")
+    ).encode("utf-8")
+    encoded_content = struct.pack("@I", len(encoded_content))
+    return {"length": encoded_content, "content": encoded_content}
 
 
-# Send an encoded message to stdout
-def sendMessage(encodedMessage):
-    sys.stdout.buffer.write(encodedMessage['length'])
-    sys.stdout.buffer.write(encodedMessage['content'])
+def send_message(encoded_message):
+    """
+    Send an encoded message to stdout
+
+    Args:
+        encoded_message: The encoded message to be sent.
+
+    Returns:
+        None
+    """
+
+    sys.stdout.buffer.write(encoded_message["length"])
+    sys.stdout.buffer.write(encoded_message["content"])
     sys.stdout.buffer.flush()
 
 
-reply_num = 0
+DOM_UPDATE_COUNTER = 0
 while True:
-    receivedMessage = getMessage()
-    reply_num += 1
-    sendMessage(encodeMessage(str(reply_num)))
-    sendMessage(encodeMessage(receivedMessage))
+    receivedMessage = get_message()
+    DOM_UPDATE_COUNTER += 1
+    send_message(encode_message(str(DOM_UPDATE_COUNTER)))
+    send_message(encode_message(receivedMessage))
