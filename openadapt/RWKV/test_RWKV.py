8d5cb00de419eff28da8195b117715141bd102e1
AvidEslami <avideslami@gmail.com>
2023-06-12 19:05:29 -0400

    allow users to run test_RWKV without modal

--- openadapt/RWKV/test_RWKV.py
+++ openadapt/RWKV/test_RWKV.py
@@ -5,11 +5,20 @@ from openadapt import config
 
 
 if __name__ == '__main__':
-    Func = modal.Function.lookup("openadapt-rwkv", "run_RWKV")
-    parameters = config.RWKV_PARAMETERS
+    if config.USE_MODAL:
+        Func = modal.Function.lookup("openadapt-rwkv", "run_RWKV")
+        parameters = config.RWKV_PARAMETERS
 
-    instruction = "You will be given a short sentence representing the start of a story."
-    task_description = "Continue the story."
-    input = "The machine learning engineer was working on a new model, when suddenly the power went out."
-    
-    print("Test:",Func.call(model=1,parameters=parameters, instruction=instruction, task_description=task_description, input=input))
+        instruction = "You will be given a short sentence representing the start of a story."
+        task_description = "Continue the story."
+        input = "The machine learning engineer was working on a new model, when suddenly the power went out."
+
+        print("Test:",Func.call(model=1,parameters=parameters, instruction=instruction, task_description=task_description, input=input))
+    else:
+        parameters = config.RWKV_PARAMETERS
+
+        instruction = "You will be given a short sentence representing the start of a story."
+        task_description = "Continue the story."
+        input = "The machine learning engineer was working on a new model, when suddenly the power went out."
+
+        print("Test:",run_RWKV(model=1,parameters=parameters, instruction=instruction, task_description=task_description, input=input))
