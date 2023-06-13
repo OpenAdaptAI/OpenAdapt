35744e932e7a06c6093f0d9ca80470df5541b307
FFFiend <owais.zahid@mail.utoronto.ca>
2023-06-13 11:58:03 -0400

    fixed formatting and other issues

--- openadapt/temp_hierarchy_script/gpt_hierarchy.py
+++ openadapt/temp_hierarchy_script/gpt_hierarchy.py
@@ -1,29 +1,25 @@
 import openai
-openai.api_key = "INSERT_API_KEY_HERE"
+import os
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role":"system",
-            "content": "You are enumerating a hierarchy of actions that \
-                 one takes when operating GUI desktop applications for typical \
-                    day-to-day tasks. Consider different levels of abstractions. \
-                        Examples include: clicking a button, opening a window, \
-                            operating payroll software, generating invoices, \
-                                renting an apartment."},
+        "content": ("You are enumerating a hierarchy of actions that "
+                    "one takes when operating GUI desktop applications for typical "
+                    "day-to-day tasks. Consider different levels of abstractions. "
+                    "Examples include: clicking a button, opening a window, "
+                    "operating payroll software, generating invoices, "
+                    "renting an apartment.")},
         {"role":"user",
-            "content":"Enumerate all of the different levels of this hierarchy, \
-                starting from the least granular to the most. Be as pedantic\
-                     as possible, down to the \
-                    key presses and mouse movements, clicks and button \
-                        presses. Format your response \
-                                    as follows: prefix each hierarchy \
-                                         level with a number, and separate \
-                                        eachh hierarchy with the word NEW.\
-                                             Make your responses as long \
-                                                    as you need them to be \
-                                                        to complete \
-                                                            your task. GO!"}
+        "content": ("Enumerate all of the different levels of this hierarchy,"
+                    "starting from the least granular to the most. Be as pedantic "
+                    "as possible, down to the key presses and mouse movements, clicks "
+                    "and button presses. Format your response as follows: prefix " 
+                    "each hierarchy level with a number, and separate each hierarchy "
+                    "with the word NEW. Make your responses as long as you need them "
+                    "to be to complete your task. GO!")}
     ]
 )
 
@@ -34,14 +30,12 @@ for i in range(len(tasks_str)):
     additional_info = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[{"role":"system",
-        "content":"You are now elaborating on a hierarchy that is part of a \
-            larger hierarchy of actions that \
-                 one takes when operating GUI desktop applications for \
-                    typical day-to-day tasks. Consider different levels \
-                        of abstractions.Examples include: clicking a button, \
-                            opening a window, \
-                            operating payroll software, generating invoices, \
-                                renting an apartment."},
+        "content": ("You are now elaborating on a hierarchy that is part of a "
+                    "larger hierarchy of actions that one takes when operating "
+                    "GUI desktop applications for typical day-to-day tasks. "
+                    "Consider different levels of abstractions.Examples include: "
+                    "clicking a button, opening a window, operating payroll "
+                    "software, generating invoices, renting an apartment.")},
                     {"role":"user",
         "content":"{}".format(temp_str)}]
     )
