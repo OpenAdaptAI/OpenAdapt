95b53f7376ed138133078b33343abd6fbc6fe91c
Owais Zahid <96851409+FFFiend@users.noreply.github.com>
2023-06-14 19:30:04 -0400

    rewrote prompt

--- openadapt/temp_hierarchy_script/gpt_hierarchy.py
+++ openadapt/temp_hierarchy_script/gpt_hierarchy.py
@@ -9,42 +9,28 @@ completion = openai.ChatCompletion.create(
         {"role":"system",
         "content": ("You are enumerating a hierarchy of actions that "
                     "one takes when operating GUI desktop applications for typical "
-                    "day-to-day tasks. Consider different levels of abstractions. "
-                    "Examples include: clicking a button, opening a window, "
+                    "day-to-day TASKS. Consider different levels of abstractions. "
+                    "Examples of such TASKS include: clicking a button, opening a window, "
                     "operating payroll software, generating invoices, "
                     "renting an apartment.")},
         {"role":"user",
-        "content": ("Enumerate all of the different levels of this hierarchy,"
-                    "starting from the least granular to the most. Be as pedantic "
+        "content": ("Enumerate all of the different levels of this hierarchy for any or all examples,"
+                    "starting from the hierarchy's least granular to the most. Be as descriptive "
                     "as possible, down to the key presses and mouse movements, clicks "
-                    "and button presses. Format your response as follows: prefix " 
-                    "each hierarchy level with a number, and separate each hierarchy "
+                    "and button presses. Format your response as follows: separate each hierarchy "
                     "with the word NEW. Make your responses as long as you need them "
                     "to be to complete your task. GO!")}
     ]
 )
 
 tasks_str = completion.choices[0].message['content'].split("NEW")
+
 tasks_by_level = {}
 for i in range(len(tasks_str)):
-    temp_str = tasks_str[i][:3000]
-    additional_info = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[{"role":"system",
-        "content": ("You are now elaborating on a hierarchy that is part of a "
-                    "larger hierarchy of actions that one takes when operating "
-                    "GUI desktop applications for typical day-to-day tasks. "
-                    "Consider different levels of abstractions.Examples include: "
-                    "clicking a button, opening a window, operating payroll "
-                    "software, generating invoices, renting an apartment.")},
-                    {"role":"user",
-        "content":"{}".format(temp_str)}]
-    )
-    
-    temp_str += additional_info.choices[0].message['content']
-    tasks_by_level[i] = temp_str
+    tasks_by_level[i] = tasks_str[i]
 
 
 if __name__ == "__main__":
     for level in tasks_by_level:
-        print(tasks_by_level)
+        print(level)
+        print(tasks_by_level[level])
