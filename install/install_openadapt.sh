a4cb0fc4f93de3ec27b4bc5140bb4b1638b5d021
Diian-r <dianrong2012@gmail.com>
2023-06-14 21:47:55 -0400

    partially down python downloading

--- install/install_openadapt.sh
+++ install/install_openadapt.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 set -e
 
-# Return true if a command/exe is available
-CheckCMDExists() {
-    command=$($1)
+# Change these if a different version of is required
+$pythonCmd="python3.10"
+$pythonVerStr="Python 3.10*"
+$pythonInstaller="python-3.10.11-macos11 21.11.47.pkg"
+$pythonInstallerLoc="https://www.python.org/ftp/python/3.10.11/python-3.10.11-macos11.pkg"
 
-    if type -P $command >/dev/null 2>&1; then
-        return 0
-    else 
-        return 1
-    fi
-}
-
-# Remove OpenAdapt
+# Remove OpenAdapt if it exists
 Cleanup(){
     if [ -d "../OpenAdapt" ]; then
         cd ..
@@ -34,14 +29,44 @@ RunAndCheck() {
     fi
 }
 
-RunAndCheck "curl --output https://www.python.org/ftp/python/3.10.11/python-3.10.11-macos11.pkg" "download Python"
+# Return true if a command/exe is available
+CheckCMDExists() {
+    command=$($1)
+
+    if type -P $command >/dev/null 2>&1; then
+        return 0
+    else 
+        return 1
+    fi
+}
+
+GetPythonCMD() {
+    # Use python alias of required version if it exists
+    if CheckCMDExists $pythonCmd; then
+        return $pythonCmd
+    fi
+
+    # Use python exe if it exists and is the required version
+    $pythonCmd="python"
+    if CheckCMDExists $pythonCmd; then
+        $res=(python --version)
+        if [[ "$res" =~ "$pythonVerStr" ]]; then
+            return $pythonCmd
+        fi
+    fi
+
+    # Install required python version
+    RunAndCheck "curl --output https://www.python.org/ftp/python/3.10.11/python-3.10.11-macos11.pkg" "Download Python"
+
+    echo "Installing python, click 'Yes' if prompted for permission"
+}
 
 [ -d "OpenAdapt" ] && mv OpenAdapt OpenAdapt-$(date +%Y-%m-%d_%H-%M-%S)
-RunAndCheck "git clone https://github.com/MLDSAI/OpenAdapt.git" "clone git repo"
+RunAndCheck "git clone https://github.com/MLDSAI/OpenAdapt.git" "Clone git repo"
 
 cd OpenAdapt
 
-RunAndCheck "python3.10 -m venv .venv" "create python virtual environment"
+RunAndCheck "python3.10 -m venv .venv" "Create python virtual environment"
 source .venv/bin/activate
 pip install wheel
 
@@ -53,6 +78,6 @@ pip install -r requirements.txt
 #   [notice] To update, run: pip install --upgrade pip
 pip install -e .
 
-RunAndCheck "alembic upgrade head" "update database"
-RunAndCheck "python3.10 -m spacy download en_core_web_trf" "download spacy model"
-RunAndCheck "pytest" "run tests"
+RunAndCheck "alembic upgrade head" "Update database"
+RunAndCheck "python3.10 -m spacy download en_core_web_trf" "Download spacy model"
+RunAndCheck "pytest" "Run tests"
