d631b2d67a53ffb46bb085f8cfbe66f6a9089aa6
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-06-15 21:53:34 -0400

    removed unused tiktoken code

--- openadapt/strategies/mixins/openai.py
+++ openadapt/strategies/mixins/openai.py
@@ -11,7 +11,6 @@ from pprint import pformat
 
 from loguru import logger
 import openai
-import tiktoken
 
 from openadapt.strategies.base import BaseReplayStrategy
 from openadapt import cache, config, models
@@ -30,7 +29,6 @@ MAX_LENGTHS = {
 MODEL_NAME = "gpt-4"
 
 openai.api_key = config.OPENAI_API_KEY
-encoding = tiktoken.get_encoding("cl100k_base")
 
 
 class OpenAIReplayStrategyMixin(BaseReplayStrategy):
@@ -160,49 +158,3 @@ def get_completion(
     logger.debug(f"appending assistant_message=\n{pformat(assistant_message)}")
     messages.append(assistant_message)
     return messages
-
-
-# XXX TODO not currently in use
-# https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb
-def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
-    """Returns the number of tokens used by a list of messages."""
-    try:
-        encoding = tiktoken.encoding_for_model(model)
-    except KeyError:
-        logger.info("Warning: model not found. Using cl100k_base encoding.")
-        encoding = tiktoken.get_encoding("cl100k_base")
-    if model == "gpt-3.5-turbo":
-        logger.info(
-            "Warning: gpt-3.5-turbo may change over time. Returning num tokens "
-            "assuming gpt-3.5-turbo-0301."
-        )
-        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
-    elif model == "gpt-4":
-        logger.info(
-            "Warning: gpt-4 may change over time. Returning num tokens "
-            "assuming gpt-4-0314."
-        )
-        return num_tokens_from_messages(messages, model="gpt-4-0314")
-    elif model == "gpt-3.5-turbo-0301":
-        tokens_per_message = (
-            4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
-        )
-        tokens_per_name = -1  # if there's a name, the role is omitted
-    elif model == "gpt-4-0314":
-        tokens_per_message = 3
-        tokens_per_name = 1
-    else:
-        raise NotImplementedError(
-            f"""num_tokens_from_messages() is not implemented for model "
-            "{model}. See "
-            "https://github.com/openai/openai-python/blob/main/chatml.md for "
-            information on how messages are converted to tokens.""")
-    num_tokens = 0
-    for message in messages:
-        num_tokens += tokens_per_message
-        for key, value in message.items():
-            num_tokens += len(encoding.encode(value))
-            if key == "name":
-                num_tokens += tokens_per_name
-    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
-    return num_tokens
