ed26aad56183d74473c5d81e442e08df2b41e45c
Diian-r <dianrong2012@gmail.com>
2023-06-19 15:55:17 -0400

    added documentation

--- openadapt/start.py
+++ openadapt/start.py
@@ -1,10 +1,19 @@
+"""
+Implements the code needed to update the OpenAdapt app
+if needed.
+
+Usage:
+    python3 -m openadapt.start
+"""
+
 import subprocess
 
 from openadapt.app.main import run_app
 
+
 def changes_needed(result: str) -> bool:
-    """ Checks if git stash needs to be run.
-    
+    """Checks if git stash needs to be run.
+
     Assumes result is the result of git status.
     """
     if "Changes to be committed:" in result:
@@ -15,15 +24,23 @@ def changes_needed(result: str) -> bool:
         return True
     return False
 
-result = subprocess.run(["git", "status"], capture_output=True, text=True)
 
-if "branch is up to date" not in result.stdout:
-    
-    if changes_needed(result.stdout):
-        subprocess.run(["git", "stash"])
-        print("Changes stashed")
-    
-    subprocess.run(["git", "pull"])
-    print("Updated the OpenAdapt App")
+def start_openadapt_app() -> None:
+    """
+    The main function which runs the OpenAdapt app when it is updated.
+    """
+    result = subprocess.run(["git", "status"], capture_output=True, text=True)
 
-run_app() # start gui
+    if "branch is up to date" not in result.stdout:
+        if changes_needed(result.stdout):
+            subprocess.run(["git", "stash"])
+            print("Changes stashed")
+
+        subprocess.run(["git", "pull"])
+        print("Updated the OpenAdapt App")
+
+    run_app()  # start gui
+
+
+if __name__ == "__main__":
+    start_openadapt_app()
