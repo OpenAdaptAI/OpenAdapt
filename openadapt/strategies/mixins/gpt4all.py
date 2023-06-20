af25939a428c98b8be5135aedbb242e862972765
Diian-r <dianrong2012@gmail.com>
2023-06-20 17:12:05 -0400

    added tokenizer and customized rpelay strategy

--- openadapt/strategies/mixins/gpt4all.py
+++ openadapt/strategies/mixins/gpt4all.py
@@ -8,11 +8,24 @@ Usage:
 """
 
 from gpt4all import GPT4All
+from transformers import AutoTokenizer
+from loguru import logger
 
 from openadapt.models import Recording
 from openadapt.strategies.base import BaseReplayStrategy
 
 MODEL_NAME = "ggml-gpt4all-j-v1.3-groovy.bin"
+# the following keys are the names of the model on GPT4All,
+#   and the value is a list with the 1st element being the name of the model on huggingface,
+#   and the 2nd element being the revision run name
+MODEL_DICTIONARY = {
+    "ggml-gpt4all-j-v1.3-groovy.bin": "gpt4all-j",
+    "ggml-replit-code-v1-3b.bin": "ggml-replit-code-v1-3b",
+    "ggml-mpt-7b-chat.bin": "gpt4all-mpt",
+    "ggml-mpt-7b-base.bin": "gpt4all-mpt",
+    "ggml-mpt-7b-instruct.bin": "gpt4all-mpt",
+    "GPT4All-13B-snoozy.ggmlv3.q4_0.bin": "gpt4all-13b-snoozy",
+}
 ROLE = "user"
 
 
@@ -21,13 +34,22 @@ class GPT4ALLReplayStrategyMixin(BaseReplayStrategy):
 
     Attributes:
         recording: the recording to be played back
-        model: the GPT4All model to be prompted
-        tokenizer: the tokenizer associated with the model
+        model: the name of the GPT4All model to be prompted
+        tokenizer: the name of the tokenizer associated with the model
     """
 
-    def __init__(self, recording: Recording, model_name: str = MODEL_NAME):
+    def __init__(
+        self, recording: Recording, model_name: str = MODEL_NAME, tokenizer: str = None
+    ):
         super().__init__(recording)
         self.GPT4All_model = GPT4All(model_name)
+        if tokenizer is None:
+            if model_name in MODEL_DICTIONARY:
+                tokenizer = "nomic-ai/" + MODEL_DICTIONARY[model_name]
+            else:
+                logger.info("There is no known tokenizer for %s", model_name)
+                return
+        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
 
     def get_gpt4all_completion(self, prompt: str, role: str = ROLE) -> str:
         """
@@ -39,10 +61,21 @@ class GPT4ALLReplayStrategyMixin(BaseReplayStrategy):
                     that describes how the message should be organized
                     (default is "user")
 
-        NOTE: for computers without GPU support, it is recommended that
+        NOTE: As indicated in logger msg, for computers without GPU support, it is recommended that
         less than 750 tokens are used for the prompt and response.
         """
+        encoded_dict = self.tokenizer.encode(prompt)
+        num_tokens = len(encoded_dict[0])
+        if num_tokens > 750:
+            logger.info(
+                "For computers without GPU support, \
+                        it is recommended that <750 tokens are used for the prompt and response. \
+                         There are %s tokens in the prompt",
+                num_tokens,
+            )
+
         messages = [{"role": role, "content": prompt}]
+
         response_dict = self.GPT4All_model.chat_completion(
             messages, default_prompt_header=False, verbose=False
         )
--- tests/openadapt/test_gpt4all.py
+++ tests/openadapt/test_gpt4all.py
@@ -1,11 +1,24 @@
-from openadapt.strategies.demo import DemoReplayStrategy
+from openadapt.strategies.mixins.gpt4all import GPT4ALLReplayStrategyMixin
 from openadapt.models import Recording
+
+
+RECORDING = Recording()
+
+
+class GPT4AllReplayStrategy(GPT4ALLReplayStrategyMixin):
+    """Custom Replay Strategy to solely test the GPT4All Mixin."""
+
+    def __init__(self, recording: Recording):
+        super().__init__(recording)
+
+    def get_next_action_event(self):
+        pass
 
 
 def test_gpt4all_initiation():
     """A smoke test for gpt4all."""
     recording = Recording()
-    replay = DemoReplayStrategy(recording)
+    replay = GPT4AllReplayStrategy(recording)
 
     response = replay.get_gpt4all_completion("Does it work?")
 
