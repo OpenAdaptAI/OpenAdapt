165184e53c010cc386087220939c73c3187d2b85
Diian-r <dianrong2012@gmail.com>
2023-06-20 13:39:54 -0400

    initial outline

---
+++ openadapt/strategies/mixins/modal_guidance.py
@@ -0,0 +1,30 @@
+import modal
+import guidance
+my_image = modal.Image.debian_slim().pip_install("guidance", "transformers", "torch", "torchvision", "torchaudio")
+
+
+stub = modal.Stub()
+
+
+@stub.function(gpu="any", image=my_image, timeout=600)
+def key_func():
+    # use alpha bc it's the smallest
+    guidance.llm = guidance.llms.Transformers("stabilityai/stablelm-base-alpha-3b")
+
+    # we can pre-define valid option sets
+    valid_medium = ["keyboard", "mouse"]
+
+    # define the prompt
+    program = guidance(""" {{description}} Given the above information, fill in the following (as N/A where unapplicable) as a valid 
+    ```json
+    {
+        "medium": "{{select 'medium' options=valid_medium}}",
+        "keyclick x-location": {{gen 'location' pattern='([0-9]+[0-9]*, [0-9]+[0-9]*)' stop=')'}},
+        "character": {{gen 'character' pattern='[a-z]+' stop=','}}
+        ]
+    }```""")
+
+    # execute the prompt
+    out = program(description="You want to close a window.", valid_medium=valid_medium)
+
+    print(out)
