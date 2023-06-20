ec1728a7f17a30335c33585d7fe55a8c926309df
dianzrong <95876281+dianzrong@users.noreply.github.com>
2023-06-20 16:58:53 -0400

    Typo

--- tests/openadapt/test_svg_mixin.py
+++ tests/openadapt/test_svg_mixin.py
@@ -12,7 +12,7 @@ RECORDING = Recording()
 
 
 class SVGReplayStrategy(SVGReplayStrategyMixin):
-    """Custom Replay Strategy to soley test the SVg Mixin."""
+    """Custom Replay Strategy to solely test the SVg Mixin."""
     def __init__(self, recording: Recording):
         super().__init__(recording)
 
