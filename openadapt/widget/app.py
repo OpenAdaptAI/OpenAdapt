d8c465dc75b4fd721e6bdc13b9b042bb397ce4d6
Jesica <jesica.susanto@mail.utoronto.ca>
2023-06-20 23:06:49 +0700

    add button

--- openadapt/widget/app.py
+++ openadapt/widget/app.py
@@ -4,11 +4,18 @@ from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.core.window import Window
 from openadapt.record import record
+import time
+import win32api
+import win32gui
+import win32con
 
 class OpenAdapt(App) :
     def build(self):
-        Window.size = (100, 100)
-        Window.clearcolor = (255,255,255)
+
+        Window.size = (200, 200)
+        Window.borderless = True
+        Window.always_on_top = True
+        Window.clearcolor = (255, 255, 255, 1)
         self.window = FloatLayout()
         self.button = Button(  background_normal="assets/logo.png",
                                         size_hint=(1, 1),
@@ -18,11 +25,16 @@ class OpenAdapt(App) :
         self.window.add_widget(self.button)
         return self.window
     def callback(self, instance):
-        self.window.remove_widget(instance)
-        self.image = Image(  source="assets/record.png",
-                                        size_hint=(1, 1),
-                                        pos_hint={"center_x" : 0.5, "center_y" : 0.5})
-        self.window.add_widget(self.image)
+        self.button.disabled = True
+        self.button.background_normal = "assets/noun-recording.png"
+        
+        # self.window.remove_widget(instance)
+        # self.image = Image(  source="assets/noun-recording.png",
+        #                                 size_hint=(1, 1),
+        #                                 pos_hint={"center_x" : 0.5, "center_y" : 0.5})
+        #self.window.add_widget(self.image)
+        #record("testing")
+        #self.image.source = "assets/noun-video-file.png"
 
 if __name__ == "__main__" :
     OpenAdapt().run()
