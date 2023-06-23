a87e20247926993821c147232d014f0cfd779e86
Diian-r <dianrong2012@gmail.com>
2023-06-23 15:12:09 -0400

    comments

--- openadapt/start.py
+++ openadapt/start.py
@@ -10,37 +10,24 @@ import subprocess
 from openadapt.app.main import run_app
 
 
-def changes_needed(result: str) -> bool:
-    """Checks if git stash needs to be run.
-
-    Assumes result is the result of git status.
-    """
-    if "Changes to be committed:" in result:
-        return True
-    elif "Changes not staged for commit:" in result:
-        return True
-    return False
-
-
-def start_openadapt_app() -> None:
+def main() -> None:
     """
     The main function which runs the OpenAdapt app when it is updated.
     """
     result = subprocess.run(["git", "status"], capture_output=True, text=True)
 
-    if changes_needed(result.stdout):
-        subprocess.run(["git", "stash"])
-
     if "unmerged" in result.stdout:
-        print("Please fix merge conflicts and try again")
+        logger.info("Please fix merge conflicts and try again")
         return
-        
-    if ("git pull" in result.stdout):
+
+    subprocess.run(["git", "stash"])
+
+    if "git pull" in result.stdout:
         subprocess.run(["git", "pull", "-q"])
-        print("Updated the OpenAdapt App")
+        logger.info("Updated the OpenAdapt App")
 
     run_app()  # start gui
 
 
 if __name__ == "__main__":
-    start_openadapt_app()
+    main()
