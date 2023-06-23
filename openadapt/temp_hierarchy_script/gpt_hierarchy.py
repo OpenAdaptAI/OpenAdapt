c62503d36f9e047cf63b3175a7162b58241e0b86
Owais Zahid <96851409+FFFiend@users.noreply.github.com>
2023-06-23 00:08:12 -0400

    used prompts in issue description

--- openadapt/temp_hierarchy_script/gpt_hierarchy.py
+++ openadapt/temp_hierarchy_script/gpt_hierarchy.py
@@ -7,30 +7,50 @@ completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role":"system",
-        "content": ("You are enumerating a hierarchy of actions that "
-                    "one takes when operating GUI desktop applications for typical "
-                    "day-to-day TASKS. Consider different levels of abstractions. "
-                    "Examples of such TASKS include: clicking a button, opening a window, "
-                    "operating payroll software, generating invoices, "
-                    "renting an apartment.")},
+            "content": ("You are enumerating a hierarchy of actions "
+                        "that one takes when operating GUI desktop "
+                        "applications for typical day-to-day tasks. "
+                        "Consider different levels of abstractions. "
+                        "Examples include: clicking a button, opening "
+                        "a window, operating payroll software, generating "
+                        " invoices, renting an apartment.")},
         {"role":"user",
-        "content": ("Enumerate all of the different levels of this hierarchy for any or all examples,"
-                    "starting from the hierarchy's least granular to the most. Be as descriptive "
-                    "as possible, down to the key presses and mouse movements, clicks "
-                    "and button presses. Format your response as follows: separate each hierarchy "
-                    "with the word NEW. Make your responses as long as you need them "
-                    "to be to complete your task. GO!")}
+            "content":("Enumerate all of the different levels of this hierarchy, "
+                        "starting from the least granular to the most. "
+                        "Only separate each level by the word NEW, do NOT use any "
+                        "new line characters between levels. Make sure that your output "
+                        "consists ONLY of levels. You are not allowed to output anything "
+                        "else.")}
     ]
 )
 
-tasks_str = completion.choices[0].message['content'].split("NEW")
+levels = completion.choices[0].message['content'].split("NEW")
 
 tasks_by_level = {}
-for i in range(len(tasks_str)):
-    tasks_by_level[i] = tasks_str[i]
+for i in range(len(levels)):
+    levelprompt = ("Enumerate all of the different tasks of this hierarchy "
+                    "at the following level: {}.".format(levels[i]))
+
+    completion_2 = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role":"system",
+                "content": ("You are enumerating a hierarchy of actions "
+                            "that one takes when operating GUI desktop "
+                            "applications for typical day-to-day tasks. "
+                            "Consider different levels of abstractions. "
+                            "Examples include: clicking a button, opening "
+                            "a window, operating payroll software, generating "
+                            " invoices, renting an apartment.")},
+            {"role":"user",
+                "content":levelprompt}
+        ]
+    )
+
+    tasks_by_level[levels[i]] = completion_2.choices[0].message['content']
 
 
-if __name__ == "__main__":
-    for level in tasks_by_level:
-        print(level)
-        print(tasks_by_level[level])
+
+for level in tasks_by_level:
+    print("This is the level {}".format(level))
+    print(tasks_by_level[level])
