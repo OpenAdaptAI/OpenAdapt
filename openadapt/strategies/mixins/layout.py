d17e35cbfe5e9e305b7b73adb0423d1b19f9cd37
FFFiend <owais.zahid@mail.utoronto.ca>
2023-06-26 10:33:01 -0400

    fixed formatting

--- openadapt/strategies/mixins/layout.py
+++ openadapt/strategies/mixins/layout.py
@@ -18,25 +18,17 @@ from openadapt.strategies.base import BaseReplayStrategy
 
 class LayoutExtractionReplayStrategyMixin(BaseReplayStrategy):
 
-    def __init__(
-        self,
-        recording: Recording,
-        img_list: List[str]
-    ):
+    def __init__(self, recording: Recording, image_file_paths: List[str]):
         super.__init__(recording)
-        self.img_list = img_list
-        
-        for i in range(len(self.img_list)):
-            wrongform = self.img_list[i]
-            self.img_list[i] = Image.open(wrongform).convert('RGB')
+        self.image_list = [
+            Image.open(img_file_path).convert("RGB")
+            for img_file_path in image_file_paths
+        ]
 
-    def document_query(
-        self, 
-        image: np.ndarray,
-        question: str
-    ) -> str:
+    def document_query(self, image: np.ndarray, question: str) -> str:
+        query_pipeline = pipeline(
+            "document-question-answering", 
+            model="impira/layoutlm-document-qa"
+        )
 
-        query_pipeline = pipeline("document-question-answering",
-            model = "impira/layoutlm-document-qa")
-
-        return query_pipeline(image, question)[0]['answer']
+        return query_pipeline(image, question)[0]["answer"]
