238ce1d026dcb7da5e6a0457c9b510f91406ab0c
Jesica <jesica.susanto@mail.utoronto.ca>
2023-06-26 11:37:48 +0700

    add kivy widget app

--- openadapt/widget/app.py
+++ openadapt/widget/app.py
@@ -1,116 +1,70 @@
-import tkinter as tk
-import signal
+import kivy
 from subprocess import Popen
-from openadapt.record import record
-import os
+import signal
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.window import Window
 
-# Initialize the current state
-current_state = "default"
 
-PROC = None
+class OpenAdaptWidget(FloatLayout):
+    def __init__(self, **kwargs):
+        super(OpenAdaptWidget, self).__init__(**kwargs)
+        self.window = Window
+        self.window.borderless = True
+        self.window.size = (200, 200)
+        self.window.clearcolor = (255, 255, 255)
+        self.window.always_on_top = True
+        self.PROC = None
+        self.button = Button(background_normal="assets/logo.png")
+        self.button.bind(on_press=self.callback)
+        self.current_state = "default"
+        self.add_widget(self.button)
 
-root = None
-def handle_click():
-    global current_state
-    global button
-    global root
-    global PROC
+    def callback(self, instance):
+        if self.current_state == "default":
+            self.button.background_normal = "assets/recording_inprogress.png"
+            self.current_state = "recording_in_progress"
+            self.start_recording()
 
-    if current_state == "default":
-        current_state = "recording_in_progress"
-        button["image"] = states[current_state]
-        start_recording()
+        elif self.current_state == "recording_in_progress":
+            self.button.background_normal = "assets/replay_available.png"
+            self.current_state = "replay_available"
+            self.stop_recording()
 
-    elif current_state == "recording_in_progress":
-        current_state = "replay_available"
-        button["image"] = states[current_state]
-        stop_recording()
+        elif self.current_state == "replay_available":
+            self.button.background_normal = "assets/replay_inprogress.png"
+            self.current_state = "replaying_in_progress"
+            self.replay_recording()
 
-    elif current_state == "replay_available":
-        current_state = "replaying_in_progress"
-        button["image"] = states[current_state]
-        button["command"] = None
-        replay_recording()
-
-    elif current_state == "replaying_in_progress":
-
-        while PROC.poll() is None:
-            # Get the current mouse position
-            # current_mouse_pos = pyautogui.position()
-
-            # Implement logic to check if the user has moved the mouse
-            # if current_mouse_pos != prev_mouse_pos:
-                # current_state = "replaying_paused"
-                # button["image"] = states[current_state]
-                # PROC.send_signal(signal.CTRL_BREAK_EVENT)
-                # break
-            break
-
-    elif current_state == "replaying_paused" :
-        PROC.send_signal(signal.CTRL_BREAK_EVENT)
-        current_state = "replaying_in_progress"
-        button["image"] = states[current_state]
-
-def start_recording():
-     global PROC
-     PROC = Popen(
+    def start_recording(self):
+        self.PROC = Popen(
             "python -m openadapt.record " + "test",
             shell=True,
         )
-     
-    
-def stop_recording():
-    global PROC
-    PROC.send_signal(signal.CTRL_C_EVENT)
 
-    # wait for process to terminate
-    PROC.wait()
-    PROC = None
+    def stop_recording(self):
+        try:
+            self.PROC.send_signal(signal.CTRL_C_EVENT)
+            # Wait for process to terminate
+            self.PROC.wait()
+        except KeyboardInterrupt:
+            # Catch the KeyboardInterrupt exception to prevent termination
+            pass
+        self.PROC = None
 
-def replay_recording():
-     global PROC
-     PROC = Popen(
-                "python -m openadapt.replay " + "NaiveReplayStrategy",
-                shell=True,
-            )
-
-def handle_exit():
-    global PROC
-    if PROC:
-        stop_recording()
-    root.destroy()
+    def replay_recording(self):
+        self.PROC = Popen(
+            "python -m openadapt.replay " + "NaiveReplayStrategy",
+            shell=True,
+        )
 
 
-def run_widget():
-    global button
-    global states
-    root = tk.Tk()
-    root.title("OpenAdapt")
-    root.geometry("300x300")
-    root.wm_attributes("-topmost", True) # make the window always on top
-    root.wm_attributes("-transparentcolor", root["bg"])
-    root.overrideredirect(True)
 
-    # Define the states
-    states = {
-        "default": tk.PhotoImage(file="./widget/assets/logo.png"),
-        "recording_in_progress": tk.PhotoImage(file="./widget/assets/noun-recording.png"),
-        "replay_available": tk.PhotoImage(file="./widget/assets/noun-replay-available.png"),
-        "replaying_in_progress": tk.PhotoImage(file="./widget/assets/noun-replaying-inprogress.png"),
-        "replaying_paused": tk.PhotoImage(file="./widget/assets/noun-pause.png")
-    }
+class OpenAdapt(App):
+    def build(self):
+        return OpenAdaptWidget()
 
-    # Create the button
-    button = tk.Button(
-        root,
-        image=states[current_state],
-        text=states[current_state],
-        command=handle_click
-    )
-    button.config( width=300, height=300 )
-    button.pack(ipadx=5, ipady=5)
-    root.protocol("WM_DELETE_WINDOW", handle_exit)
-    root.mainloop()
 
 if __name__ == "__main__":
-    run_widget()
+    OpenAdapt().run()
