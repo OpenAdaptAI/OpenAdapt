204f56c321093327386922253f8fef076f68a096
Krish Patel <kmppatel133@gmail.com>
2023-06-28 11:39:03 -0400

    add background,js

---
+++ extension/background.js
@@ -0,0 +1,25 @@
+let port = null;  // Native Messaging port
+const hostName = 'openadapt';
+
+
+/*
+ * Handle received messages
+*/
+function onReceived(response) {
+  console.log(response);
+}
+
+/*
+ * Handle disconnection from native app
+*/
+function onDisconnectedFromNativeHost() {
+  console.log('Disconnected from native host');
+  port = null;
+}
+
+
+port = chrome.runtime.connectNative(hostName);
+port.onMessage.addListener(onReceived);
+port.onDisconnect.addListener(onDisconnectedFromNativeHost);
+console.log('Connected to native messaging host: ' + hostName);
+port.postMessage({ text: "Hello, my_application" });
