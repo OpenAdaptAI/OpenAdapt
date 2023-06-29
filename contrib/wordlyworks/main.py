a04e08da72ad7c7a78194e682e43cbf4a0328388
Richard Abrich <richard.abrich@gmail.com>
2023-06-29 13:01:00 -0400

    working

--- contrib/wordlyworks/main.py
+++ contrib/wordlyworks/main.py
@@ -6,10 +6,14 @@ Generate combined SVG
 Run through e.g. Stable Diffusion and/or ask large language model to generate animation code
 """
 
+from pprint import pformat
+from requests_oauthlib import OAuth1
 import os
+import requests
 
 from dotenv import load_dotenv
 from loguru import logger
+#from TheNounProjectAPI import API
 
 load_dotenv()
 
@@ -18,6 +22,22 @@ def main():
     key = os.environ["NOUN_PROJECT_KEY"]
     secret = os.environ["NOUN_PROJECT_SECRET"]
     logger.info(f"{key=} {secret=}")
+
+
+    auth = OAuth1(key, secret)
+    endpoint = "https://api.thenounproject.com/v2/icon/1"
+
+    response = requests.get(endpoint, auth=auth)
+    logger.info(f"response=\n{pformat(response.json())}")
+
+
+    if 0:
+        api = API(key=key, secret=secret)
+        icons = api.get_icons_by_term("goat", public_domain_only=True, limit=2)
+        for icon in icons:
+            print("Icon's term:", icon.term)
+            print("This icon's tags:", ", ".join(tag.slug for tag in icon.tags))
+            print("Uploader's username:", icon.uploader.username)
 
 
 if __name__ == "__main__":
