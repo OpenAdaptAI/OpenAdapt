aa44f3848a8b40b53412dd8b8372375e46ce5416
Krish Patel <kmppatel133@gmail.com>
2023-06-30 13:21:00 -0400

    Mutation Observer in content.js

--- native_chrome_extension/content.js
+++ native_chrome_extension/content.js
@@ -1,20 +1,35 @@
-// Function to extract relevant information from the document
-function extractDocumentInfo() {
-  const documentInfo = {
-    title: document.title,
-    url: window.location.href,
-    html: document.documentElement.outerHTML
-  };
-  return documentInfo;
+let port = null;  // Native Messaging port
+const hostName = 'openadapt';
+
+
+/*
+ * Handle received messages
+*/
+function onReceived(response) {
+  console.log(response);
 }
 
-// Send the document info to the background script
-function sendDocumentInfo() {
-    const documentInfo = extractDocumentInfo();
-    chrome.runtime.sendMessage({ type: 'document', documentInfo });
+/*
+ * Handle disconnection from native app
+*/
+function onDisconnectedFromNativeHost() {
+  console.log('Disconnected from native host');
+  port = null;
 }
 
 
-sendDocumentInfo();  // Send the initial document info
+port = chrome.runtime.connectNative(hostName);
+port.onMessage.addListener(onReceived);
+port.onDisconnect.addListener(onDisconnectedFromNativeHost);
+console.log('Connected to native messaging host: ' + hostName);
+port.postMessage({ text: "Hello, my_application" });
 
-document.addEventListener('DOMSubtreeModified', sendDocumentInfo);  // Listen for DOM changes and send the updated document info
+/*
+* Forward messages from content scripts to the native app
+*/
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === 'logDOMChange') {
+    port.postMessage(message.documentInfo);
+    console.log('Message forwarded to native app');
+  }
+});
