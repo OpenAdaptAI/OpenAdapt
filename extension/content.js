0b373a97d08825f4c650015f24303bde4a19e803
Krish Patel <kmppatel133@gmail.com>
2023-06-30 12:58:36 -0400

    update content script to use DOM mutatinos

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,32 @@
-chrome.runtime.sendMessage("hi");
+/*
+ * Function to send a message to the background script
+*/
+function sendMessageToBackgroundScript(message) {
+    chrome.runtime.sendMessage(message);
+  }
+  
+  
+/*
+* Function to detect DOM changes
+*/
+function detectDOMChanges() {
+// Create a mutation observer
+const observer = new MutationObserver(function(mutationsList) {
+    // Send a message to the background script when a DOM change is detected
+    sendMessageToBackgroundScript({
+    action: 'logDOMChange',
+    documentObject: document,
+    });
+});
+
+// Start observing DOM changes
+observer.observe(document, {
+    subtree: true,
+    childList: true,
+    attributes: true,
+    characterData: true,
+});
+}
+
+// Call the function to start detecting DOM changes
+detectDOMChanges();
