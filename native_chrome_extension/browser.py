6105dbd43ea6186308fa2ca7728b390c732f97e1
Krish Patel <kmppatel133@gmail.com>
2023-07-03 11:04:32 -0400

    style: ran black on browser.py and add module docstring

--- native_chrome_extension/browser.py
+++ native_chrome_extension/browser.py
@@ -16,6 +16,7 @@ from namedpipe import NPopen
 # Credit and Reference:
 # https://github.com/Rayquaza01/nativemessaging/blob/master/nativemessaging/
 
+
 def getMessage() -> Any:
     """
     Read a message from stdin and decode it.
@@ -30,8 +31,8 @@ def getMessage() -> Any:
     rawLength = sys.stdin.buffer.read(4)
     if len(rawLength) == 0:
         sys.exit(0)
-    messageLength = struct.unpack('@I', rawLength)[0]
-    message = sys.stdin.buffer.read(messageLength).decode('utf-8')
+    messageLength = struct.unpack("@I", rawLength)[0]
+    message = sys.stdin.buffer.read(messageLength).decode("utf-8")
     return json.loads(message)
 
 
@@ -39,43 +40,46 @@ def encodeMessage(messageContent: Any) -> Dict[str, bytes]:
     """
     Encode a message for transmission,
     given its content.
-    
+
     Args:
         messageContent: The content of the message to encode.
-    
+
     Returns:
         A dictionary containing the encoded message.
     """
 
     # https://docs.python.org/3/library/json.html#basic-usage
-    # To get the most compact JSON representation, you should specify 
+    # To get the most compact JSON representation, you should specify
     # (',', ':') to eliminate whitespace.
-    # We want the most compact representation because the browser rejects # messages that exceed 1 MB.
-    encodedContent = json.dumps(messageContent, separators=(',', ':')).encode('utf-8')
-    encodedLength = struct.pack('@I', len(encodedContent))
-    return {'length': encodedLength, 'content': encodedContent}
+    # We want the most compact representation because the browser rejects
+    # # messages that exceed 1 MB.
+    encodedContent = json.dumps(messageContent, separators=(",", ":")).encode(
+        "utf-8"
+    )
+    encodedLength = struct.pack("@I", len(encodedContent))
+    return {"length": encodedLength, "content": encodedContent}
 
 
 def sendMessage(encodedMessage: Dict[str, bytes]) -> None:
     """
     Send an encoded message to stdout
-    
+
     Args:
         encodedMessage: The encoded message to send.
-    
+
     Returns:
         None
     """
 
-    sys.stdout.buffer.write(encodedMessage['length'])
-    sys.stdout.buffer.write(encodedMessage['content'])
+    sys.stdout.buffer.write(encodedMessage["length"])
+    sys.stdout.buffer.write(encodedMessage["content"])
     sys.stdout.buffer.flush()
 
 
 def server():
     """
-    The main function of the browser-side native messaging host/server.__annotations__
-    
+    The main function of the browser-side native messaging host/server
+
     Args:
         None
 
