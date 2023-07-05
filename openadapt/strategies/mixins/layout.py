24d38694f27843b6ce09ada36e511530e9c8f636
FFFiend <owais.zahid@mail.utoronto.ca>
2023-07-04 21:02:30 -0400

    changed equality syntax

--- openadapt/strategies/mixins/layout.py
+++ openadapt/strategies/mixins/layout.py
@@ -31,7 +31,7 @@ class LayoutExtractionReplayStrategyMixin(BaseReplayStrategy):
             model="impira/layoutlm-document-qa"
         )
         output = query_pipeline(image, question)
-        if output != []:
+        if not output:
             return query_pipeline(image, question)[0]['answer']
         return "Unsupported document type, please input a text-based document"
     
