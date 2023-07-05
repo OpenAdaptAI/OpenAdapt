ab442d4abef8994388c437f7a54e649bbafa02f1
AvidEslami <avideslami@gmail.com>
2023-07-05 16:04:57 -0400

    created signal test similar to those used for RWKV

--- openadapt/test_vectordb.py
+++ openadapt/test_vectordb.py
@@ -32,3 +32,19 @@ results = collection.query(
 
 print(results["ids"])
 print(collection.get(results["ids"][0])["documents"])
+
+# Signal Test
+
+signals = [{'id': 0, 'type': 'file', 'descriptor': 'restaurant_menu_data.txt'},{'id': 1, 'type': 'url', 'descriptor': 'https://en.wikipedia.org/wiki/Web_development'},{'id': 2, 'type': 'function', 'descriptor': 'math.sqrt'},{'id': 3, 'type': 'url', 'descriptor': 'https://www.accuweather.com'},{'id': 4, 'type': 'database', 'descriptor': 'footwear.db'},{'id': 5, 'type': 'function', 'descriptor': 'openai.Completion.create'},{'id': 6, 'type': 'file', 'descriptor': 'anatomy_data.csv'}]
+
+collection.add(
+    documents=[str(signals[0]),str(signals[1]),str(signals[2]),str(signals[3]),str(signals[4]),str(signals[5]),str(signals[6])],
+    ids=["signal 0","signal 1","signal 2","signal 3","signal 4","signal 5","signal 6"]
+)
+
+results = collection.query(
+    query_texts=["Creating a website about shoes."],
+    n_results=2
+)
+
+print(results["ids"])
