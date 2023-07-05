0d744a51030c882f94db46dce0f1eaa86dafde87
FFFiend <owais.zahid@mail.utoronto.ca>
2023-07-04 21:05:11 -0400

    removed test pipelien class

--- tests/openadapt/test_layout.py
+++ tests/openadapt/test_layout.py
@@ -5,24 +5,6 @@ from PIL import Image
 from transformers import pipeline
 import numpy as np
 
-
-class TestQAPipeline:
-    def __init__(self, image_file_paths: List[str]):
-        self.image_list = [
-            Image.open(img_file_path).convert("RGB")
-            for img_file_path in image_file_paths
-        ]
-
-    def document_query(self, image: np.ndarray, question: str) -> str:
-        query_pipeline = pipeline(
-            "document-question-answering", model="impira/layoutlm-document-qa"
-        )
-        output = query_pipeline(image, question)
-        if output != []:
-            return query_pipeline(image, question)[0]["answer"]
-        return "Unsupported document type, please input a text-based document"
-
-
 IMAGE_FILE_NAMES = [
     "test_ladingbill.png",
     "test_invoice.png",
