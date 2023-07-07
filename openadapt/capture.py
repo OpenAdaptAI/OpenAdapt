e45dfa60c3613e05287f080dd31b287616132be2
Aaron <57018940+0dm@users.noreply.github.com>
2023-07-07 15:30:28 -0400

    add camera

--- openadapt/capture.py
+++ openadapt/capture.py
@@ -25,13 +25,14 @@ class Capture:
 
         objc.options.structs_indexable = True
 
-    def start(self, audio=False):
+    def start(self, audio=False, camera=False):
         self.display_id = CGMainDisplayID()
         self.session = AVF.AVCaptureSession.alloc().init()
         self.screen_input = AVF.AVCaptureScreenInput.alloc().initWithDisplayID_(
             self.display_id
         )
         self.file_output = AVF.AVCaptureMovieFileOutput.alloc().init()
+        self.camera_session = None  # not used if camera=False
 
         # Create an audio device input with the default audio device
         self.audio_input = AVF.AVCaptureDeviceInput.alloc().initWithDevice_error_(
@@ -48,6 +49,7 @@ class Capture:
         )
         if audio and self.session.canAddInput_(self.audio_input[0]):
             self.session.addInput_(self.audio_input[0])
+
         if self.session.canAddInput_(self.screen_input):
             self.session.addInput_(self.screen_input)
 
@@ -62,12 +64,42 @@ class Capture:
             )
         )
 
+        if camera:
+            self._use_camera()
+
+    def _use_camera(self):
+        self.camera_session = AVF.AVCaptureSession.alloc().init()
+        self.camera_file_output = AVF.AVCaptureMovieFileOutput.alloc().init()
+        self.camera_input = AVF.AVCaptureDeviceInput.alloc().initWithDevice_error_(
+            AVF.AVCaptureDevice.defaultDeviceWithMediaType_(AVF.AVMediaTypeVideo), None
+        )
+
+        if self.camera_session.canAddInput_(self.camera_input[0]):
+            self.camera_session.addInput_(self.camera_input[0])
+        self.camera_session.startRunning()
+
+        self.camera_session.addOutput_(self.camera_file_output)
+
+        self.camera_url = (
+            self.camera_file_output.startRecordingToOutputFileURL_recordingDelegate_(
+                NSURL.fileURLWithPath_(
+                    os.path.join(
+                        config.CAPTURE_DIR_PATH,
+                        datetime.now().strftime("camera.%Y-%m-%d-%H-%M-%S") + ".mov",
+                    )
+                ),
+                NSObject.alloc().init(),
+            )
+        )
+
     def stop(self):
         self.session.stopRunning()
+        if self.camera_session:
+            self.camera_session.stopRunning()
 
 
 if __name__ == "__main__":
     capture = Capture()
-    capture.start(audio=True)
+    capture.start(audio=True, camera=True)
     input("Press enter to stop")
     capture.stop()
