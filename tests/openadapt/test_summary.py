6d83b7f158b116afb24958582ec0b6a38cbf788b
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2023-07-07 10:41:18 -0400

    revert test_summary.py

--- tests/openadapt/test_summary.py
+++ tests/openadapt/test_summary.py
@@ -2,10 +2,12 @@
 Tests the summarization function in summary.py
 """
 from fuzzywuzzy import fuzz
-import pytest
 
 from openadapt.strategies.mixins.summary import SummaryReplayStrategyMixin
 from openadapt.models import Recording
+
+
+RECORDING = Recording()
 
 
 class SummaryReplayStrategy(SummaryReplayStrategyMixin):
@@ -18,24 +20,20 @@ class SummaryReplayStrategy(SummaryReplayStrategyMixin):
         pass
 
 
-@pytest.fixture(scope="module")
-def replay_fixture(setup_database):
-    recording = Recording()  # Use the Recording class from your code
-    setup_database()  # Set up the database
-    replay = setup_database(recording)
-    yield replay
+REPLAY = SummaryReplayStrategy(RECORDING)
 
 
-def test_summary_empty(replay_fixture):
+def test_summary_empty():
     empty_text = ""
-    actual = replay_fixture.get_summary(empty_text, 1)
+    actual = REPLAY.get_summary(empty_text, 1)
     assert len(actual) == 0
 
 
-def test_summary_sentence(replay_fixture):
+def test_summary_sentence():
     story = "However, this bottle was not marked “poison,” so Alice ventured to taste it, \
         and finding it very nice, (it had, in fact, a sort of mixed flavour of cherry-tart, \
         custard, pine-apple, roast turkey, toffee, and hot buttered toast,) \
         she very soon finished it off."
-    actual = replay_fixture.get_summary(story, 1)
+    actual = REPLAY.get_summary(story, 1)
     assert fuzz.WRatio(actual, story) > 50
+    
