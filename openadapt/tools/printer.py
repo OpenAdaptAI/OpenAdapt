2fafad679976ce2b1c7d4c6569561c6d205bf042
Jesica <jesica.susanto@mail.utoronto.ca>
2023-07-10 10:10:46 -0400

    PrinterIntegration class

--- openadapt/tools/printer.py
+++ openadapt/tools/printer.py
@@ -1,39 +1,55 @@
 import sys
-from PySide6.QtCore import QCoreApplication, Qt
-from PySide6.QtWidgets import QApplication
-from PySide6.QtPrintSupport import QPrinterInfo, QPrintDialog
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMessageBox, QComboBox
+from PyQt5.QtPrintSupport import QPrinter, QPrintDialog
 
-def get_available_printers():
-    printer_info_list = QPrinterInfo.availablePrinters()
-    printer_names = [printer.printerName() for printer in printer_info_list]
-    return printer_names
+class PrinterIntegration:
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        
+    def get_available_printers(self):
+        printer_info = QPrinter()
+        printer_names = printer_info.supportedPrinterNames()
+        return printer_names
 
-def print_document(document_path, printer_name):
-    app = QCoreApplication.instance()
-    if app is None:
-        app = QApplication(sys.argv)
-    
-    printer_info = QPrinterInfo.printerInfo(printer_name)
-    if printer_info is not None:
-        printer = QPrinter(printer_info)
+    def print_document(self, document_path, printer_name):
+        printer = QPrinter()
         printer.setOutputFormat(QPrinter.NativeFormat)
         printer.setOutputFileName(document_path)
+        printer.setPrinterName(printer_name)
         
         print_dialog = QPrintDialog(printer)
-        print_dialog.setWindowTitle("Print Document")
-        print_dialog.setOptions(QPrintDialog.PrintToFile | QPrintDialog.PrintPageRange)
-        
-        if print_dialog.exec() == QPrintDialog.Accepted:
-            painter = printer.paintEngine().painter()
+        if print_dialog.exec_() == QPrintDialog.Accepted:
+            printer.printFile(document_path)
             
-            painter = QtGui.QPainter(printer)
-            painter.begin(printer)
-            
-            painter.end()
-        
-        if isinstance(app, QApplication):
-            sys.exit(app.exec())
+        sys.exit(self.app.exec())
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    sys.exit(app.exec())
+    printer_integration = PrinterIntegration()
+    
+    document_path, _ = QFileDialog.getOpenFileName(None, "Select Document to Print", "", "PDF Files (*.pdf);;All Files (*.*)")
+    
+    if document_path:
+        printer_names = printer_integration.get_available_printers()
+        
+        if printer_names:
+            printer_combo = QComboBox()
+            printer_combo.addItems(printer_names)
+            printer_combo.setCurrentIndex(0)
+            printer_combo_dialog = QMessageBox()
+            printer_combo_dialog.setWindowTitle("Select Printer")
+            printer_combo_dialog.setText("Select the printer:")
+            printer_combo_dialog.setIcon(QMessageBox.Question)
+            printer_combo_dialog.addButton(QMessageBox.Cancel)
+            printer_combo_dialog.addButton(QMessageBox.Ok)
+            printer_combo_dialog.setDefaultButton(QMessageBox.Ok)
+            printer_combo_dialog.setEscapeButton(QMessageBox.Cancel)
+            printer_combo_dialog.setFixedWidth(300)
+            printer_combo_dialog.layout().addWidget(printer_combo)
+            
+            if printer_combo_dialog.exec_() == QMessageBox.Ok:
+                selected_printer = printer_combo.currentText()
+                printer_integration.print_document(document_path, selected_printer)
+        else:
+            QMessageBox.warning(None, "Error", "No printers available.")
+    else:
+        QMessageBox.warning(None, "Error", "No document selected.")
