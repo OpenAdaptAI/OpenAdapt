af9391e60a415d2316a703df6e2d41807aa6d7f8
Nathan Weston <nweston@fastmail.com>
2023-07-11 17:54:40 -0400

    fixup! fix(utils): prevent duplicate log messages (#339)

--- openadapt/utils.py
+++ openadapt/utils.py
@@ -32,10 +32,10 @@ def configure_logging(logger, log_level):
     with _logger_lock:
         logger.remove()
         logger_format = (
-            "{time:YYYY-MM-DD HH:mm:ss.SSS} | "
-            "{level: <8} | "
-            "{name}:{function}:{line} "
-            "- {message}"
+            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | "
+            "<level>{level: <8}</level> | "
+            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> "
+            "- <level>{message}</level>"
         )
         logger.add(
             StreamHandler(sys.stderr),
