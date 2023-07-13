02931b1ae0ca731fce96ffce051cc059204516ee
FFFiend <owais.zahid@mail.utoronto.ca>
2023-07-13 00:08:34 -0400

    added davinci_pipeline methods

--- openadapt/finetune.py
+++ openadapt/finetune.py
@@ -1,5 +1,4 @@
-from openadapt import models
-from openadapt.crud import get_recording_by_id
+from openadapt import crud, models
 from loguru import logger
 import fire
 
@@ -7,7 +6,7 @@ VALID_MODELS = ("davinci", "mpt-7b")
 
 
 def main(recording_id: int, model_name: str):
-    finetune_recording = get_recording_by_id(recording_id)
+    finetune_recording = crud.get_recording_by_id(recording_id)
     if not finetune_recording:
         raise ValueError("A recording with the given id does not exist")
 
@@ -15,6 +14,31 @@ def main(recording_id: int, model_name: str):
         raise ValueError("This model is not supported for finetuning yet")
 
     logger.debug(f"{finetune_recording=}")
+
+
+def davinci_learn_recording_summary_pipeline(
+    summary: str, finetune_recording: models.Recording
+):
+    # prompt: the recording window states and recordings distilled
+    # completion: summary of the recording
+    summary_window_states = crud.get_window_events(finetune_recording)
+    summary_ref_action_events = crud.get_action_events(finetune_recording)
+
+    prompt = (f"{summary_window_states}\n",f"{summary_ref_action_events}")
+    completion = summary
+
+    logger.debug(f"{prompt=}")
+    logger.debug(f"{completion=}")
+
+    # TODO
+
+
+def davinci_get_next_action_event_pipeline():
+    # given the summary of an incomplete recording, along 
+    # with the window states and action events, 
+    # ask the model to generate "next event" completions
+    
+    # TODO
 
 
 if __name__ == "__main__":
