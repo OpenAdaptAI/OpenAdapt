e40a94498e4c5b72c0f1428248c38cad6a3bf6a6
FFFiend <owais.zahid@mail.utoronto.ca>
2023-07-12 23:08:05 -0400

    added some stylistic changes

--- openadapt/finetune.py
+++ openadapt/finetune.py
@@ -3,9 +3,7 @@ from openadapt.crud import get_recording_by_id
 from loguru import logger
 import fire
 
-VALID_MODELS = set()
-VALID_MODELS.add("Davinci")
-VALID_MODELS.add("MPT-7B")
+VALID_MODELS = ("davinci", "mpt-7b")
 
 
 def main(recording_id: int, model_name: str):
@@ -13,7 +11,7 @@ def main(recording_id: int, model_name: str):
     if not finetune_recording:
         raise ValueError("A recording with the given id does not exist")
 
-    if model_name not in VALID_MODELS:
+    if model_name.lower() not in VALID_MODELS:
         raise ValueError("This model is not supported for finetuning yet")
 
     logger.debug(f"{finetune_recording=}")
