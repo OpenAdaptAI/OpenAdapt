f18747e5d3ee1642c8033279c827368765edd270
FFFiend <owais.zahid@mail.utoronto.ca>
2023-07-15 17:08:06 -0400

    added assert statements

--- openadapt/finetune.py
+++ openadapt/finetune.py
@@ -5,13 +5,18 @@ import fire
 VALID_MODELS = ("davinci", "mpt-7b")
 
 
+class EventType:
+    WINDOW = "WINDOW"
+    ACTION = "ACTION"
+
+
 def main(recording_id: int, model_name: str):
     finetune_recording = crud.get_recording_by_id(recording_id)
-    if not finetune_recording:
-        raise ValueError("A recording with the given id does not exist")
 
-    if model_name.lower() not in VALID_MODELS:
-        raise ValueError("This model is not supported for finetuning yet")
+    assert finetune_recording, "A recording with the given id does not exist"
+    assert (
+        model_name.lower() not in VALID_MODELS
+    ), "This model is not supported for finetuning yet"
 
     logger.debug(f"{finetune_recording=}")
 
@@ -24,33 +29,39 @@ def davinci_learn_recording_summary_pipeline(
     summary_window_states = crud.get_window_events(finetune_recording)
     summary_ref_action_events = crud.get_action_events(finetune_recording)
 
-    prompt = (f"{summary_window_states}\n",f"{summary_ref_action_events}")
+    prompt = (f"{summary_window_states}\n", f"{summary_ref_action_events}")
     completion = summary
 
     logger.debug(f"{prompt=}")
     logger.debug(f"{completion=}")
 
-    
     sanitized_window_states = event_sanitizer(summary_window_states, "window")
     sanitized_ref_action_events = event_sanitizer(summary_ref_action_events, "action")
-    
+
     # write to a json file in the format
     # {prompt: finetune_prompt, completion: incomplete_recording}
     # where finetune_prompt is the same as in get_next_action_event in stateful
-    
+
     # TODO
 
 
 def davinci_get_next_action_event_pipeline():
-    # given the summary of an incomplete recording, along 
-    # with the window states and action events, 
+    # given the summary of an incomplete recording, along
+    # with the window states and action events,
     # ask the model to generate "next event" completions
-    
+
+    # For now, this method must only be called after guaranteeing that we have 
+    # a certain number of recordings stored in the json file (i.e we have finetuned
+    # on enough examples.)
     # TODO
     pass
 
-def event_sanitizer(event, type: str = "window" | "action"):
-    pass
+
+def event_sanitizer(event, event_type: EventType):
+    assert (
+        event_type.upper() == EventType.WINDOW or event_type.upper() == EventType.ACTION
+    ), "Not a valid event type"
+
 
 if __name__ == "__main__":
     fire.Fire(main)
