6be1f1bc80a9c95418ff07e3d1d277eb91323a3e
Jesica <jesica.susanto@mail.utoronto.ca>
2023-07-17 09:38:02 -0400

    add test_printer.py

---
+++ tests/openadapt/test_printer.py
@@ -0,0 +1,8 @@
+import pytest
+from openadapt.tools import printer
+
+@pytest.mark.skipif(not printer.get_available_printers(), reason="No printers available")
+def test_print_document():
+    printer_name = printer.get_available_printers()[0]  # Use the first available printer
+    file_path = "assets/test_print_document.pdf"  # Replace with an actual test file
+    printer.print_document(printer_name, file_path)
