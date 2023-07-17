d8aaee07bc920a07c741c53a257ea52f1a05f97f
Jesica <jesica.susanto@mail.utoronto.ca>
2023-07-17 09:57:19 -0400

    black

--- tests/openadapt/test_printer.py
+++ tests/openadapt/test_printer.py
@@ -5,10 +5,13 @@ from openadapt.tools import printer
 # Get the directory of the current test script
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
+
 @pytest.mark.skipif(
     not printer.get_available_printers(), reason="No printers available"
 )
 def test_print_document():
-    printer_name = printer.get_available_printers()[0]  # Use the first available printer
+    printer_name = printer.get_available_printers()[
+        0
+    ]  # Use the first available printer
     file_path = os.path.join(current_dir, "resources", "test_print_document.pdf")
     printer.print_document(printer_name, file_path)
