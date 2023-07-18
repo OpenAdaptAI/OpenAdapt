e53152698fdafae5aa0bde2b4efdec324bae5834
Diian-r <dianrong2012@gmail.com>
2023-07-17 23:40:53 -0400

    make nicer

--- openadapt/strategies/mixins/lmql_action.py
+++ openadapt/strategies/mixins/lmql_action.py
@@ -1,3 +1,9 @@
+"""Implements the LMQL library to restrict model outputs.
+
+Typical usage example:
+    class MyReplayStrategy(SummaryReplayStrategyMixin):
+        ...
+"""
 import re
 import string
 
@@ -5,26 +11,40 @@ import lmql
 from loguru import logger
 from pynput.keyboard import Key
 
-from openadapt.strategies.base import BaseReplayStrategy
-from openadapt.models import Recording, ActionEvent
 from openadapt.config import ACTION_TEXT_NAME_PREFIX, ACTION_TEXT_NAME_SUFFIX
+from openadapt.models import Recording, ActionEvent
+from openadapt.strategies.base import BaseReplayStrategy
 
 DEFAULT_MODEL = "openai/text-davinci-003"
-REGEX = re.compile('[%s]' % re.escape(string.punctuation))
+REGEX = re.compile("[%s]" % re.escape(string.punctuation))
+
 
 class LMQLReplayStrategyMixin(BaseReplayStrategy):
+    """ReplayStrategy Mixin which restricts model output.
+
+    Attributes:
+        - recording: the recording to be played back
+        - model_name: the model to be prompted
+    """
 
     def __init__(
         self,
         recording: Recording,
         model_name: str = DEFAULT_MODEL,
     ):
+        """
+        See base class.
+
+        Additional Attribute:
+            - model_name: the model to be prompted
+        """
         super().__init__(recording)
 
         logger.info(f"{model_name=}")
         self.model_name = model_name
 
     def get_valid_action_event(self, prompt: str) -> ActionEvent:
+        """Return the ActionEvent the model returns from the prompt."""
         list_of_results = lmql_call(description=prompt, model=self.model_name)
         result = list_of_results[0]
         logger.info(f"The filled in prompt is: {result.prompt}")
@@ -33,11 +53,12 @@ class LMQLReplayStrategyMixin(BaseReplayStrategy):
         action = parse_action(result_dict)
         return action
 
+
 @lmql.query
-def lmql_call(description: str, model: str) -> list:
+def lmql_call(prompt: str, model: str) -> list:
     '''lmql
     argmax
-        "Summarize the following description as a json: {description} {{ "
+        "Summarize the following description as a json: {prompt} {{ "
         "'action': [ACTION], "
         if ACTION == "press" or ACTION == "release":
             "'key': [KEY] }}"
@@ -56,14 +77,16 @@ def lmql_call(description: str, model: str) -> list:
     from
         model
     where
-        ACTION in set(["move", "scroll", "press", "release", "click"]) 
-        and len(TOKENS(KEY)) < 5 and STOPS_BEFORE(KEY, " }") 
-        and len(TOKENS(KEY2)) < 5 and STOPS_BEFORE(KEY2, " }") 
-        and INT(X) and INT(Y) and len(TOKENS(X)) < 5 and len(TOKENS(Y)) < 5 
+        ACTION in set(["move", "scroll", "press", "release", "click"])
+        and len(TOKENS(KEY)) < 5 and STOPS_BEFORE(KEY, " }")
+        and len(TOKENS(KEY2)) < 5 and STOPS_BEFORE(KEY2, " }")
+        and INT(X) and INT(Y) and len(TOKENS(X)) < 5 and len(TOKENS(Y)) < 5
         and CLICK in set(["left", "right"])
     '''
 
+
 def parse_action(result_dict: dict) -> ActionEvent:
+    """A helper function to turn the output dictionary into a valid ActionEvent."""
     name = result_dict["ACTION"]
     logger.info(f"The action is: {name}")
     action_event = ActionEvent(name=name)
@@ -74,13 +97,15 @@ def parse_action(result_dict: dict) -> ActionEvent:
         if type_of_key(key) == "invalid":
             output2 = result_dict["KEY2"]
             key = clean_output(output2)
-        if key[0] == ACTION_TEXT_NAME_PREFIX and key [-1] == ACTION_TEXT_NAME_SUFFIX:
+
+        if key[0] == ACTION_TEXT_NAME_PREFIX and key[-1] == ACTION_TEXT_NAME_SUFFIX:
             key_name = key[1:-1]
             action_event.key_name = key_name
-        if len(key) == 1:
+            action_event.canonical_key_name = key_name
+        elif len(key) == 1:
             action_event.key_char = key
             action_event.canonical_key_char = key
-        if hasattr(Key, key):
+        elif hasattr(Key, key):
             action_event.key_name = key
             action_event.canonical_key_name = key
         else:
@@ -97,20 +122,25 @@ def parse_action(result_dict: dict) -> ActionEvent:
         action_event.mouse_x = result_dict["X"]
         action_event.mouse_y = result_dict["Y"]
         action_event.mouse_button_name = result_dict["CLICK"]
-    
+
     return action_event
+
 
 def type_of_key(key: str) -> str:
     """Return a string representing what type of key the input is.
-    
+
     Types of keys:
         - char: a single character
         - key_name: a multicharacter key
         - vk: a virtual key
         - invalid: not a valid key
     """
-    if key[0] == ACTION_TEXT_NAME_PREFIX and key [-1] == ACTION_TEXT_NAME_SUFFIX and hasattr(Key, key[1:-1]):
-         return "key_name"
+    if (
+        key[0] == ACTION_TEXT_NAME_PREFIX
+        and key[-1] == ACTION_TEXT_NAME_SUFFIX
+        and hasattr(Key, key[1:-1])
+    ):
+        return "key_name"
     elif len(key) == 1:
         return "char"
     elif hasattr(Key, key):
@@ -123,8 +153,9 @@ def type_of_key(key: str) -> str:
             return "invalid"
         return "vk"
 
+
 def clean_output(output: str) -> str:
     """Remove the whitespace and punctuation."""
     output_without_whitespace = output.strip()
-    clean_output = REGEX.sub('', output_without_whitespace)
+    clean_output = REGEX.sub("", output_without_whitespace)
     return clean_output
