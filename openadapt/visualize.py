7a8313ad958a354bfb0b4f25552c48cc2b620f8d
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2023-07-18 13:32:33 -0400

    ran black

--- openadapt/visualize.py
+++ openadapt/visualize.py
@@ -188,7 +188,7 @@ def dict2html(
             html_str = head + middle + tail
     return html_str
 
-  
+
 @logger.catch
 def main() -> None:
     """Main function to generate an HTML report for a recording."""
