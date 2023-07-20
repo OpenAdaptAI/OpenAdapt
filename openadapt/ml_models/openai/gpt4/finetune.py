b2b1eb21a267e9e976dbf5e4a9bb178ae1fc9dd6
FFFiend <owais.zahid@mail.utoronto.ca>
2023-07-20 07:00:00 -0400

    added some more code to finetune.py

--- openadapt/ml_models/openai/gpt4/finetune.py
+++ openadapt/ml_models/openai/gpt4/finetune.py
@@ -1,6 +1,22 @@
+from openadapt import models
+from openadapt.crud import get_action_events, get_window_events
 import fire
 import openai
-def main():
+
+class EventType:
+    WINDOW = "WINDOW"
+    ACTION = "ACTION"
+
+def main(recording: models.Recording):
+    # sanitize, convert to json, feed to model
+    act_dicts = get_action_events(recording)
+    win_dicts = get_window_events(recording)
+    
+    sanitized_act_dict = sanitize_dicts("ACTION", act_dicts)
+    sanitized_win_dict = sanitize_dicts("WINDOW", win_dicts)
+    pass
+
+def sanitize_dicts(event_type: EventType, event):
     pass
 
 if __name__ == "__main__":
