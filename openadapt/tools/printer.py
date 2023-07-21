aa647054685db2441cc807372acf362ac2ce72cf
Jesica <jesica.susanto@mail.utoronto.ca>
2023-07-21 10:43:21 -0400

    add get_printer_jobs_on_windows

--- openadapt/tools/printer.py
+++ openadapt/tools/printer.py
@@ -28,3 +28,40 @@ def print_document(printer_name, file_path):
         win32api.ShellExecute(0, "print", file_path, f'/d:"{printer_name}"', ".", 0)
     else:
         subprocess.run(["lp", "-d", printer_name, file_path], capture_output=True)
+
+
+def get_printer_jobs(printer_name):
+    if platform.system() == "Windows":
+        # Get printer jobs on Windows (Same as the previous implementation)
+        phandle = win32print.OpenPrinter(printer_name)
+        print_jobs = win32print.EnumJobs(phandle, 0, -1, 1)
+        win32print.ClosePrinter(phandle)
+        return print_jobs
+    else:
+        # Get printer jobs on non-Windows platforms using lpstat
+        result = subprocess.run(
+            ["lpstat", "-W", "completed", "-o", printer_name],
+            capture_output=True,
+            text=True,
+        )
+        output = result.stdout.strip().split("\n")
+        jobs = []
+        for line in output:
+            job_info = line.split()
+            job = {
+                "job_id": int(job_info[0]),
+                "username": job_info[1],
+                "total_pages": int(job_info[2]),
+                "pages_printed": int(job_info[3]),
+                "status": job_info[4],
+            }
+            jobs.append(job)
+        return jobs
+
+
+if __name__ == "__main__":
+    import ipdb
+
+    ipdb.set_trace()
+    printer_name = get_available_printers()[0]
+    get_printer_jobs(printer_name)
--- tests/openadapt/test_printer.py
+++ tests/openadapt/test_printer.py
@@ -1,11 +1,31 @@
 import pytest
 import os
+import platform
 from openadapt.tools import printer
+
+
 # Get the directory of the current test script
 current_dir = os.path.dirname(os.path.abspath(__file__))
+printer_list = printer.get_available_printers()[0]
 
-@pytest.mark.skipif(not printer.get_available_printers(), reason="No printers available")
+
+@pytest.mark.skipif(
+    not printer.get_available_printers(), reason="No printers available"
+)
 def test_print_document():
-    printer_name = printer.get_available_printers()[0]  # Use the first available printer
+    printer_name = printer.get_available_printers()[0]
     file_path = os.path.join(current_dir, "resources", "test_print_document.pdf")
     printer.print_document(printer_name, file_path)
+
+
+@pytest.mark.skipif(
+    not printer.get_available_printers(), reason="No printers available"
+)
+def test_get_printer_jobs_on_windows():
+    # Mock the win32print.EnumJobs function for Windows platform
+    printer_name = printer.get_available_printers()[0]
+    printer_jobs = printer.get_printer_jobs(printer_name)
+
+
+if __name__ == "__main__":
+    pytest.main()
