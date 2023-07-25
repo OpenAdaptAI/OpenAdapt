5e9521badb1cd140bff764b87d3ca707bd14912b
Krish Patel <kmppatel133@gmail.com>
2023-07-25 15:21:06 -0400

    update llama2

---
+++ openadapt/ml_models/karpathy/llama2c/__init__.py
@@ -0,0 +1,10 @@
+"""A ML model `llama2.c` provider module.
+
+    Usage: `openadapt.ml.karpathy.llama2c`
+"""
+
+from ml_models import Modality
+import finetune
+import infer
+
+MODALITIES = [Modality.TEXT]
