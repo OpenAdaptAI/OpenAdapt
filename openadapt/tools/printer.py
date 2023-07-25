f95ee6aeb21ce9be6f3b5194e0287c154fbfbfb8
jesicasusanto <jesica.ssnt@gmail.com>
2023-07-25 09:56:03 -0700

    test print on non windows

--- openadapt/tools/printer.py
+++ openadapt/tools/printer.py
@@ -1,6 +1,6 @@
 import subprocess
 import platform
-
+import os
 
 def get_available_printers():
     printers = []
@@ -30,8 +30,10 @@ def print_document(printer_name, file_path):
     if platform.system() == "Windows":
         win32api.ShellExecute(0, "print", file_path, f'/d:"{printer_name}"', ".", 0)
     else:
-        subprocess.run(["lp", "-d", printer_name, file_path], capture_output=True)
-
+        result = subprocess.run(["lp", "-d", printer_name, file_path], capture_output=True, text=True)
+        result = result.stdout
+        job_id = result.strip().split()[3]
+        return job_id
 
 def get_printer_jobs(printer_name):
     if platform.system() == "Windows":
@@ -47,8 +49,20 @@ def get_printer_jobs(printer_name):
             ["lpstat", "-o", printer_name],
             capture_output=True,
         )
-        return result
-
-
+        result = result.stdout.decode('utf-8')
+        #import ipdb; ipdb.set_trace()
+        lines = result.strip().split('\n')
+        job_ids= [line.split()[0] for line in lines]
+        return job_ids
 if __name__ == "__main__":
-    get_available_printers()
+    # Get the directory of the current test script
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    printer_name = get_available_printers()[0]
+    file_path = '/Users/jesicasusanto/Desktop/MLDSAI/PAT/tests/openadapt/resources/test_print_document.pdf'
+    job_id = print_document(printer_name, file_path) 
+    job_ids = get_printer_jobs(printer_name)
+    import ipdb; ipdb.set_trace()
+    if platform == "Windows" :
+        pass
+    else :
+        assert job_id in job_ids
