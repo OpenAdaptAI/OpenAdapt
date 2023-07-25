bbdff48f9afe11b13aa44afe46defa9dbca1edaa
Krish Patel <kmppatel133@gmail.com>
2023-07-25 13:25:39 -0400

    add ml.providers.llama2.c.py

---
+++ openadapt/ml/providers/llama2.c.py
@@ -0,0 +1,13 @@
+"""A ML model `llama2.c` provider module."""
+
+
+# TODO: Implement the following functions.
+
+
+def infer():
+    raise NotImplementedError
+
+
+# TODO: Implement the following functions.
+def fine_tune():
+    raise NotImplementedError
