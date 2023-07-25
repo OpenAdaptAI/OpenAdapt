4c28f62e943949f2179763d4d340dd8806c8bd2f
Mustafa Abdulrahman <mus2003.abdul@gmail.com>
2023-07-24 23:31:14 -0400

    fix: resolve annotation errors and restore incorrectly removed code in lint PR

--- openadapt/strategies/__init__.py
+++ openadapt/strategies/__init__.py
@@ -3,8 +3,8 @@
 Module: __init__.py
 """
 
-# from openadapt.strategies.base import BaseReplayStrategy
-# from openadapt.strategies.naive import NaiveReplayStrategy
-# from openadapt.strategies.demo import DemoReplayStrategy
+from openadapt.strategies.base import BaseReplayStrategy
+from openadapt.strategies.demo import DemoReplayStrategy
+from openadapt.strategies.naive import NaiveReplayStrategy
 
 # add more strategies here
