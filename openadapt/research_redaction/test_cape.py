cbabfe4ae1bac0827aa6f12516d7c6aaf0252569
Krish Patel <kmppatel133@gmail.com>
2023-07-28 12:39:27 -0400

    test cape for pdfs (llc)

--- openadapt/research_redaction/test_cape.py
+++ openadapt/research_redaction/test_cape.py
@@ -4,15 +4,11 @@ import requests
 
 from openadapt import config
 
-resp = requests.post(
-    "https://api.capeprivacy.com/v1/privacy/deidentify/text",
-    headers={"Authorization": f"Bearer {config.CAPE_API_KEY}"},
-    json={
-        "content": (
-            "There were three bids for the painting: David bid 30.000€, Larissa bid"
-            " 35.000€, and Mark bid 37.000€."
-        )
-    },
-)
+with open("./files/sample_llc_2.pdf", "rb") as f:
+    resp = requests.post(
+        "https://api.capeprivacy.com/v1/privacy/deidentify/file",
+        headers={"Authorization": f"Bearer {config.CAPE_API_KEY}"},
+        files={"file": f},
+    )
 
 print(resp.json())
