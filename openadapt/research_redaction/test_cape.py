9f2b2c1588dead52c57dfd98ba0d68d5b656fcdb
Krish Patel <kmppatel133@gmail.com>
2023-07-31 11:18:57 -0400

    try to redact pdfs

--- openadapt/research_redaction/test_cape.py
+++ openadapt/research_redaction/test_cape.py
@@ -12,3 +12,18 @@ with open("./files/sample_llc_2.pdf", "rb") as f:
     )
 
 print(resp.json())
+
+# import os
+
+# import requests
+
+# from openadapt import config
+
+# with open("./files/sample_emr_1.png", "rb") as f:
+#     resp = requests.post(
+#         "https://api.capeprivacy.com/v1/privacy/deidentify/file",
+#         headers={"Authorization": f"Bearer {config.CAPE_API_KEY}"},
+#         files={"file": f.read()},
+#     )
+
+# print(resp.json())
