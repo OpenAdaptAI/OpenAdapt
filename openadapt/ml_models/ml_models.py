abd5522d2155805061b35d134ed561cd80d7b194
FFFiend <owais.zahid@mail.utoronto.ca>
2023-07-31 20:25:10 -0400

    added completion provider base class

--- openadapt/ml_models/ml_models.py
+++ openadapt/ml_models/ml_models.py
@@ -1,14 +1,5 @@
 from openadapt import models
-import openai_module
-from openai_module.gpt3_davinci import infer, finetune, MODALITIES
-from openadapt.crud import (
-    get_recording_by_id,
-    get_latest_recording,
-    get_window_events,
-    get_action_events,
-)
-from loguru import logger 
-
+from pydantic import BaseModel
 
 class Modality:
     TEXT = "TEXT"
@@ -24,35 +15,13 @@ class Capability:
     USABLE_OUTPUT = "USABLE_OUTPUT"
     INFERENCE = "INFERENCE"
 
-MODALITY_BY_DB_MODEL = {
-    models.ActionEvent: Modality.TEXT,
-    models.Screenshot: Modality.IMAGE,
-    models.WindowEvent: Modality.IMAGE,
-}
+class CompletionProvider(BaseModel):
+    Name: str
+    Capabilities: list[Capability]
+    Modalities: list[Modality]
 
-ML_MODELS = [
-    {
-        "name": "gpt3_davinci",
-        "provider": openai_module.gpt3_davinci,
-        "finetune": finetune,
-        "infer": infer,
-        "modalities": MODALITIES,
-        "context_length": 4096,
-        "license": License.COMMERCIAL_USE,
-        "capability": [Capability.TRAINING, Capability.TUNING, Capability.USABLE_OUTPUT, Capability.INFERENCE]
-    }
-]
+    def infer(self, prompt: str):
+        raise NotImplementedError
 
-
-def select_ml_model(recording_id):
-    recording_data = get_recording_by_id(recording_id)
-    assert recording_data, "No such recording exists"
-    # TODO:
-    # for model in ML_MODELS:
-    #    # use wandb for metrics on different ml models.
-
-
-def add_model(name: str, provider: str):
-    """
-    High level function to add a new model to the list of ML modes
-    """
+    def finetune(self, prompt: str, completion: str):
+        raise NotImplementedError
--- openadapt/ml_models/openai_module/gpt3_davinci/infer.py
+++
@@ -1,7 +0,0 @@
-import fire
-import openai
-def main(prompt: str):
-    pass
-
-if __name__ == "__main__":
-    fire.Fire(main)
