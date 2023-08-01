2dd0d37cbc2cd5c1834fc23255802fc1974ef43a
FFFiend <owais.zahid@mail.utoronto.ca>
2023-07-31 21:03:40 -0400

    added correct attributes to gpt class

--- openadapt/ml_models/openai_module/gpt3_davinci/gpt3provider.py
+++ openadapt/ml_models/openai_module/gpt3_davinci/gpt3provider.py
@@ -1,7 +1,15 @@
-from ml_models import CompletionProvider
+from base_provider import CompletionProvider, Modality, Capability
+
 
 class GPT3CompletionProvider(CompletionProvider):
-    name = "GPT3-Davinci"
+    Name = "GPT3-Davinci"
+    Capabilities = [
+        Capability.TRAINING,
+        Capability.TUNING,
+        Capability.USABLE_OUTPUT,
+        Capability.INFERENCE,
+    ]
+    Modalities = [Modality.TEXT]
 
     def finetune(self, prompt: str, completion: str):
         # TODO
@@ -10,5 +18,3 @@ class GPT3CompletionProvider(CompletionProvider):
     def infer(self, prompt: str):
         # TODO add call to local finetuned model for inference
         pass
-
-        
