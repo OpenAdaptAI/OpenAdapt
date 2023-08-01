fe6d728057e6266555a6872a9e89b54d93fcf2e6
FFFiend <owais.zahid@mail.utoronto.ca>
2023-07-31 20:59:05 -0400

    added completion_Factory class

---
+++ openadapt/ml_models/completion_factory.py
@@ -0,0 +1,7 @@
+
+from base_provider import Modality, CompletionProvider
+class CompletionProviderFactory:
+    def get_for_modality(modality: Modality) -> list[CompletionProvider]:
+        completion_providers = CompletionProvider.get_children()
+        filtered_providers = [provider for provider in completion_providers if modality in provider.Modalities]
+        return filtered_providers
