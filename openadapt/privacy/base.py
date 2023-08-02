52f3301f0f3bdfc6ad1b2eafc19067a40c1c321b
Krish Patel <kmppatel133@gmail.com>
2023-08-02 12:04:55 -0400

    update base.py

--- openadapt/privacy/base.py
+++ openadapt/privacy/base.py
@@ -23,19 +23,61 @@ class ScrubbingProvider(BaseModel):
     name: str
     capabilities: List[str]
 
-    class Config:
-        arbitrary_types_allowed = True
-
     def scrub_text(self, text: str, is_separated: bool = False) -> str:
+        """Scrub the text of all PII/PHI.
+
+        Args:
+            text (str): Text to be scrubbed
+            is_separated (bool): Whether the text is separated with special characters
+
+        Returns:
+            str: Scrubbed text
+        """
         raise NotImplementedError
 
     def scrub_image(
         self, image: Image, fill_color: int = config.SCRUB_FILL_COLOR
     ) -> Image:
+        """Scrub the image of all PII/PHI.
+
+        Args:
+            image (Image): A PIL.Image object to be scrubbed
+            fill_color (int): The color used to fill the redacted regions(BGR).
+
+        Returns:
+            Image: The scrubbed image with PII and PHI removed.
+        """
         raise NotImplementedError
 
-    def scrub_pdf(self):
+    def scrub_pdf(self, path_to_pdf: str) -> str:
+        """Scrub the PDF of all PII/PHI.
+
+        Args:
+            path_to_pdf (str): Path to the PDF to be scrubbed
+
+        Returns:
+            str: Path to the scrubbed PDF
+        """
         raise NotImplementedError
 
-    def scrub_mp4(self):
+    def scrub_mp4(
+        self,
+        mp4_file: str,
+        scrub_all_entities: bool = False,
+        playback_speed_multiplier: float = 1.0,
+        crop_start_time: int = 0,
+        crop_end_time: int | None = None,
+    ):
+        """Scrub a mp4 file.
+
+        Args:
+            mp4_file_path: Path to the mp4 file.
+            scrub_all_entities: True/False. If true, scrubs all entities
+            playback_speed_multiplier: Multiplier for playback speed. (float/int)
+            crop_start_time: Start Time (in seconds)
+            end_start_time: End Time (in seconds)
+
+        Returns:
+            Path to the scrubbed (redacted) mp4 file.
+        """
         raise NotImplementedError
