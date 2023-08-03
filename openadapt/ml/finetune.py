c68d3e69ac87aac5eff2e0fe70dc8bc2d738b3fa
FFFiend <owais.zahid@mail.utoronto.ca>
2023-08-03 19:49:44 -0400

    completed condense_window_state function

--- openadapt/ml/finetune.py
+++ openadapt/ml/finetune.py
@@ -6,63 +6,34 @@ from loguru import logger
 from openadapt.strategies.stateful import get_window_state_diffs
 
 
-def condense_window_state(recording_id):
+def condense_window_state(recording_id: int):
+    """
+    Takes in a recording as input and outputs a list of tuples
+    of action, window events where window events are condensed
+    to contain basic information, as in stateful_test.py
+    """
     grab_recording = get_recording_by_id(recording_id)
-    curr_acx = get_action_events(grab_recording)
+    total_acx = get_action_events(grab_recording)
 
-    diffs = get_window_state_diffs(curr_acx)
-    logger.debug(f"{diffs=}")
-    return
+    for i in range(len(total_acx)):
+        processed_acx, processed_wd = (
+            sanitize(total_acx[i])[0],
+            sanitize(total_acx[i])[1],
+        )
+        processed_wd.pop("meta")
+        # task_description = recording.task_description
 
-    for action in curr_acx:
-        processed_acx, processed_wd = sanitize(action)[0], sanitize(action)[1]
-        logger.debug(f"{processed_acx=}")
-        logger.debug(f"{processed_wd=}")
+        total_acx[i] = (processed_acx, processed_wd)
 
-        break
-        task_description = recording.task_description
-        summary_dataset(processed_wd_axvalue, task_description)
-
-    # get task description
+    logger.debug(total_acx)
+    return total_acx
 
 
-# What to fine tune on
+def finetune(recording_id: int):
+    condensed_recording = condense_window_state(recording_id)
 
-# if we finetune on actions, then the model can perform
-# actions on windows where these actions are meaningless.
-# => BAD IDEA.
-
-# things that are a problem: length of reference window states.
-
-# right now, we give it reference window and action dict for ONE TASK
-
-# then we give it an active window state and ask it to replicate these
-# actions on the current window.
-
-
-# we absolutely NEED the coordinates of the generated action
-# to match the ones in the active window state.
-
-# SINCE the window doesnt change, how about just describing the
-# translation, and saving up on that length?
-
-# then diffs for the reference window state.
-########################################################################
-# Goal: we wish to enhance statefulreplaystrat to work properly.
-
-
-def summary_dataset(processed_wd_axvalue, task_desciption: str):
-    pass
-    # write to a json file with format
-    # {prompt: axvalue, completion: task_description}
-
-
-def finetune_on_summary(prompt: str, completion: str):
-    # prompt is the current ACTION's PAIRED WINDOW STATE,
-    # and completion is the task description of the recording
-
-    pass
-    # read each line in json file, and train model on it.
+    for curr_acx, curr_window in condensed_recording:
+        pass
 
 
 def sanitize(action):
@@ -106,11 +77,7 @@ def sanitize(action):
             )
         ]
     reference_window_dict["state"].pop("data")
-    reference_window_dict.pop("left")
-    reference_window_dict.pop("top")
-    reference_window_dict.pop("width")
-    reference_window_dict.pop("height")
-    return reference_action_dicts, reference_window_dict
+    return reference_action_dicts, reference_window_dict["state"]
 
 
 if __name__ == "__main__":
