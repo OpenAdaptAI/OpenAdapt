42a46c5212290dfeddb36ee81a9c354a73bc1a09
Krish Patel <kmppatel133@gmail.com>
2023-08-10 00:33:57 -0400

    update tests  to include scrubbing for Date_TIME and URL

--- tests/openadapt/test_scrub.py
+++ tests/openadapt/test_scrub.py
@@ -2,12 +2,9 @@
 
 from io import BytesIO
 import os
-import warnings
 
-from loguru import logger
 from PIL import Image
 import cv2
-import numpy
 import pytesseract
 import requests
 
@@ -24,10 +21,10 @@ def _hex_to_rgb(hex_color: int) -> tuple[int, int, int]:
         tuple[int, int, int]: RGB values.
     """
     assert 0x000000 <= hex_color <= 0xFFFFFF
-    b = (hex_color >> 16) & 0xFF
-    g = (hex_color >> 8) & 0xFF
-    r = hex_color & 0xFF
-    return r, g, b
+    black = (hex_color >> 16) & 0xFF
+    green = (hex_color >> 8) & 0xFF
+    red = hex_color & 0xFF
+    return red, green, black
 
 
 def _enhance_image_for_ocr(input_image_path: str, enhanced_image_path: str) -> None:
@@ -41,31 +38,34 @@ def _enhance_image_for_ocr(input_image_path: str, enhanced_image_path: str) -> N
         None
     """
     # Covert image to 300 DPI for best OCR results with Tesseract
-    # Reference: https://tesseract-ocr.github.io/tessdoc/ImproveQuality.html#:~:text=Tesseract%20works%20best%20on%20images,of%20capital%20letter%20in%20pixels.
-    im = Image.open(input_image_path)
-    im.save(enhanced_image_path, dpi=(300, 300))
+    # Reference:
+    # https://tesseract-ocr.github.io/tessdoc/ImproveQuality.html#:~:text=Tesseract%20works%20best%20on%20images,of%20capital%20letter%20in%20pixels. # pylint: disable=line-too-long # noqa: E501
+    image = Image.open(input_image_path)
+    image.save(enhanced_image_path, dpi=(300, 300))
 
-    image_read = cv2.imread(enhanced_image_path)
+    image_read = cv2.imread(enhanced_image_path)  # pylint: disable=no-member
     os.remove(enhanced_image_path)
 
     # Develop contrast for better OCR results with Tesseract
-    # Reference: https://stackoverflow.com/questions/58314275/how-to-make-image-more-contrast-grayscale-then-get-all-characters-exactly-with
+    # Reference: https://stackoverflow.com/questions/58314275/how-to-make-image-more-contrast-grayscale-then-get-all-characters-exactly-with # pylint: disable=line-too-long # noqa: E501
 
-    gray_image = cv2.cvtColor(
-        image_read, cv2.COLOR_BGR2GRAY
+    gray_image = cv2.cvtColor(  # pylint: disable=no-member
+        image_read, cv2.COLOR_BGR2GRAY  # pylint: disable=no-member
     )  # developing contrast for better OCR results with Tesseract
-    thr_image = thr = cv2.adaptiveThreshold(
-        gray_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 23, 100
+    thr_image = cv2.adaptiveThreshold(  # pylint: disable=no-member
+        gray_image,
+        255,
+        cv2.ADAPTIVE_THRESH_MEAN_C,  # pylint: disable=no-member
+        cv2.THRESH_BINARY_INV,  # pylint: disable=no-member
+        23,
+        100,
     )
-    bnt_image = cv2.bitwise_not(thr_image)
-    cv2.imwrite(enhanced_image_path, bnt_image)
+    bnt_image = cv2.bitwise_not(thr_image)  # pylint: disable=no-member
+    cv2.imwrite(enhanced_image_path, bnt_image)  # pylint: disable=no-member
 
 
 def test_emr_image() -> None:
-    """
-    Test to that a sample EMR image is scrubbed.
-    """
-
+    """Test to verify that an EMR scrubbed image does not contain any PII/PHI."""
     test_image_path = "assets/test_emr_image.png"
     test_image_path_300 = "assets/test_emr_image_300.png"
     scrubbed_image_path = test_image_path[:-4] + "_scrubbed.png"
@@ -73,8 +73,6 @@ def test_emr_image() -> None:
     _enhance_image_for_ocr(test_image_path, test_image_path_300)
     original_image_text = pytesseract.image_to_string(Image.open(test_image_path_300))
     os.remove(test_image_path_300)
-
-    logger.debug(original_image_text)
 
     # Preparing for Scrubbing
     with open(test_image_path, "rb") as file:
@@ -96,9 +94,13 @@ def test_emr_image() -> None:
     os.remove(scrubbed_image_path)
 
     # Use Cape to detect PII/PHI in the scrubbed image ocr text
-    resp = requests.post(
+    resp = requests.post(  # pylint: disable=missing-timeout
         "https://api.capeprivacy.com/v1/privacy/deidentify/text",
-        headers={"Authorization": f"Bearer {config.CAPE_API_KEY}"},
+        headers={
+            "Authorization": (
+                f"Bearer {config.CAPE_API_KEY}"  # pylint: disable=no-member
+            )
+        },
         json={
             "content": scrubbed_image_text,
         },
@@ -111,7 +113,6 @@ def test_emr_image() -> None:
 
 def test_scrub_image() -> None:
     """Test that the scrubbed image data is different."""
-
     # Read test image data from file
     test_image_path = "assets/test_scrub_image.png"
     with open(test_image_path, "rb") as file:
@@ -135,7 +136,7 @@ def test_scrub_image() -> None:
     mask_pixels = sum(
         1
         for pixel in scrubbed_image.getdata()
-        if pixel == _hex_to_rgb(config.SCRUB_FILL_COLOR)
+        if pixel == _hex_to_rgb(config.SCRUB_FILL_COLOR)  # pylint: disable=no-member
     )
     total_pixels = scrubbed_image.width * scrubbed_image.height
 
@@ -145,7 +146,7 @@ def test_scrub_image() -> None:
 
     # Assert ~1.5% mask pixels compared to total pixels.
     assert (
-        round((mask_pixels / total_pixels), 3) == 0.015
+        round((mask_pixels / total_pixels), 3) == 0.022
     )  # Change this value as necessary
 
 
@@ -190,7 +191,7 @@ def test_scrub_date_of_birth() -> None:
     """Test that the date of birth is scrubbed."""
     assert (
         scrub.scrub_text("My date of birth is 01/01/2000.")
-        == "My date of birth is 01/01/2000."
+        == "My date of birth is <DATE_TIME>."
     )
 
 
@@ -268,9 +269,5 @@ def test_scrub_all_together() -> None:
         " his phone number is <PHONE_NUMBER>."
         "His credit card number is <CREDIT_CARD> and"
         " his social security number is <US_SSN>."
-        " He was born on 01/01/1980."
+        " He was born on <DATE_TIME>."
     )
-
-
-if __name__ == "__main__":
-    test_scrub_image()
