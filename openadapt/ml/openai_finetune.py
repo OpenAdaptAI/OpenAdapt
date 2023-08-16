2f557278803e96fa29777645b71c8ffd1c8bd262
FFFiend <owais.zahid@mail.utoronto.ca>
2023-08-16 16:54:28 -0400

    added write to file method

--- openadapt/ml/openai_finetune.py
+++ openadapt/ml/openai_finetune.py
@@ -1,6 +1,8 @@
 from openadapt.ml.fine_tuning.base_finetuner import FineTuner
 from typing import Optional
+import json
 import subprocess
+
 
 class OpenAIFineTuner(FineTuner):
     def __init__(self, model_name) -> None:
@@ -20,41 +22,57 @@ class OpenAIFineTuner(FineTuner):
             capture_output=True,
         )
         return output.stdout.decode("utf-8")
-    
+
     def tune_model(self, fine_tune_data_path: Optional[str] = None):
         output = subprocess.run(
-            ["openai", "api", "fine_tunes.create", "-t", fine_tune_data_path, "-m", self.model_name],
+            [
+                "openai",
+                "api",
+                "fine_tunes.create",
+                "-t",
+                fine_tune_data_path,
+                "-m",
+                self.model_name,
+            ],
             capture_output=True,
         )
         return output.stdout.decode("utf-8")
 
-"""
-def write_to_file(recording_id: int):
-    
-    condensed_recording = condense_window_state(recording_id)
-    # for curr_acx, curr_window in condensed_recording:
-    #   curr_pair= f'action:{curr_acx}, window: {curr_window}'
-    #   pair_json = json.loads(curr_pair)
-    recording_file = open(f"{recording_id}_processed.jsonl", "x")
-    recording_file.close()
-    with open(
-        f"{recording_id}_processed.jsonl",
-        mode="w",
-    ) as json_file:
-        for idx in range(len(condensed_recording) - 1):
-            curr_acx = condensed_recording[idx][0][0]
-            curr_win = condensed_recording[idx][1]
+    def write_to_file(self, recording_id: int) -> str:
+        """
+        users must invoke this to prepare their data into a JSONL file
+        formatted according to OpenAI Finetune standards. The function
+        returns the file path which can be passed into prepare_data_for_tuning.
+        """
+        condensed_recording = self.data_processor._condense_data(recording_id)
 
-            next_acx = condensed_recording[idx + 1][0][0]
-            next_win = condensed_recording[idx + 1][1]
-            paired_dict = {
-                "prompt": f"{(curr_acx, curr_win)}",
-                "completion": f" {(next_acx,next_win)}",
-            }
-            # write this to a file
-            # paired_dict_json = json.loads(str(paired_dict))
-            json_file.write(json.dumps(paired_dict))
-            json_file.write("\n")
+        file_path = generate_file_path(recording_id)
 
-        json_file.close()
-"""
+        recording_file = open(file_path, "x")
+        recording_file.close()
+        with open(
+            recording_file,
+            mode="w",
+        ) as json_file:
+            for curr_event, next_event in zip(
+                condensed_recording, condensed_recording[1:]
+            ):
+                curr_acx = curr_event[0][0]
+                curr_win = curr_event[1]
+
+                next_acx = next_event[0][0]
+                next_win = next_event[1]
+                paired_dict = {
+                    "prompt": f"{(curr_acx, curr_win)}",
+                    "completion": f" {(next_acx,next_win)}",
+                }
+                json_file.write(json.dumps(paired_dict))
+                json_file.write("\n")
+
+            json_file.close()
+
+        return file_path
+
+
+def generate_file_path(recording_id: int) -> str:
+    return f"{recording_id}_processed.jsonl"
