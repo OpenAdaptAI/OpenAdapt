5f235fb8fb41a07bbbe8e9e94beac0bad9940488
angelala3252 <88949118+angelala3252@users.noreply.github.com>
2023-08-18 01:37:45 -0700

    create image and window state dataset

--- openadapt/ml/data/synthesize.py
+++ openadapt/ml/data/synthesize.py
@@ -1,31 +1,61 @@
-from openadapt.crud import (
-    get_latest_recording,
-    get_window_events
-)
+import json
+import os
 
-# TODO: temp
-questions = ["What is the position of the button that closes the window? "
-             "Is it on the right, left, top, or bottom side of the screen?"]
+from openadapt.crud import get_latest_recording, get_window_events
+
+PATH_TO_IMAGES = "openadapt/ml/data/vision_dataset/images"
+PATH_TO_JSON = "openadapt/ml/data/vision_dataset/states.json"
 
 
 def synthesize():
     """
-    TODO: fix
-    - get latest recording
-    - get all window events states
-    - get associated screenshot
-    - filter out unneeded info (meta/data)
-    - split
-    - create dataset
+    Creates dataset of screenshots and their respective window states from the latest recording.
+    Filters out unneeded info from window states (meta, data, window_id).
+    Saves screenshot with a unique ID, and saves the window states along with the ID to a JSON file.
     """
     recording = get_latest_recording()
     window_events = get_window_events(recording)
-    image_to_state = {}
+    states = []
+
+    existing_ids = [
+        int(filename.split(".")[0])
+        for filename in os.listdir(PATH_TO_IMAGES)
+        if filename.split(".")[0].isdigit()
+    ]
+    image_id = max(existing_ids, default=0) + 1
+
+    if os.path.isfile(PATH_TO_JSON):
+        with open(PATH_TO_JSON, "r") as json_file:
+            existing_json = json.load(json_file)
+    else:
+        existing_json = []
+
     for event in window_events:
         state = event.state
+
+        if state is None:
+            state = {
+                "title": event.title,
+                "left": event.left,
+                "top": event.top,
+                "width": event.width,
+                "height": event.height,
+            }
+        else:
+            del state["meta"]
+            del state["data"]
+            del state["window_id"]
+
         # take last action event's screenshot
-        ss = event.action_events[-1].screenshot
-        image_to_state[ss] = state
+        ss = event.action_events[-1].screenshot.image
+        ss.save(f"{PATH_TO_IMAGES}/{image_id}.jpg")
+        new_state_entry = {"id": image_id, "window_state": state}
+        states.append(new_state_entry)
+        image_id += 1
+
+    existing_json.extend(states)
+    with open(PATH_TO_JSON, "w") as json_file:
+        json.dump(existing_json, json_file)
 
 
 def get_event_screenshots():
@@ -38,31 +68,5 @@ def get_event_screenshots():
     return screenshots
 
 
-def ask_models():
-    import subprocess
-    subprocess.run(["modal", "run", "openadapt/minigpt4.py"])
-
-
-# TODO: generate questions to ask the model on the screenshots and put into a spreadsheet
-def write_to_spreadsheet(answers, images):
-    import openpyxl
-
-    # Load the workbook
-    workbook = openpyxl.Workbook()
-    sheet = workbook.active
-
-    # Write data to the sheet
-    data = []
-    for i in range(0, len(answers)):
-        data.append([questions[0], answers[i]])
-        # TODO: images
-
-    for row in data:
-        sheet.append(row)
-
-    # Save the workbook
-    workbook.save("example.xlsx")
-
-
 if __name__ == "__main__":
-    write_to_spreadsheet(["Hi"], [])
+    synthesize()
