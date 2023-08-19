3f715bb44d1796a85dd9a1a242926125f199045c
FFFiend <owais.zahid@mail.utoronto.ca>
2023-08-18 20:38:16 -0400

    removed _create_pipeline method

--- openadapt/ml/huggingface_models/generic_provider.py
+++ openadapt/ml/huggingface_models/generic_provider.py
@@ -22,9 +22,11 @@ class GenericHuggingFaceProvider(CompletionProvider):
         model = AutoModel.from_pretrained(model_name)
         return model
 
-    def _create_pipeline(model_name: str, task: str, trust_remote_code=False):
+    def infer(prompt: str, model_name: str, task_description: str, use_pipeline=True, trust_remote_code=False):
         """
-        Initializer and returns a pipeline object using the
+        Infers on a model of the user's choice, using Huggingface pipelines. 
+
+        Initializes a pipeline object to infer from using the
         given model and its corresponding task description.
 
         The trust_remote_code parameter must be set to true if the model
@@ -33,23 +35,12 @@ class GenericHuggingFaceProvider(CompletionProvider):
         An example of a model that requires trust_remote_code=True is
         MPT-7b-Instruct.
         """
-        hf_pipeline = pipeline(
-            task=task, model=model_name, trust_remote_code=trust_remote_code
-        )
-        return hf_pipeline
-
-    def infer(prompt: str, model_name: str, task_description: str, use_pipeline=True):
-        """
-        Infers on the model of the user's choice. Currently resorting to
-        defaulting on using HF pipelines for the inference.
-        """
         assert use_pipeline and task_description, "Please enter a task description."
-        if use_pipeline and task_description:
-            inference_pipeline = GenericHuggingFaceProvider._create_pipeline(
-                model_name=model_name, task=task_description
-            )
-            inference_output = inference_pipeline(prompt)
-            return inference_output
+        inference_pipeline = pipeline(
+            model_name=model_name, task=task_description, trust_remote_code=trust_remote_code
+        )
+        inference_output = inference_pipeline(prompt)
+        return inference_output
 
     def save_model():
         raise NotImplementedError
