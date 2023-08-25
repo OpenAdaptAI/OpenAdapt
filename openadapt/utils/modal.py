73248ce04b53cc48107e1deb8961a2ac0bf7f32c
FFFiend <owais.zahid@mail.utoronto.ca>
2023-08-25 04:06:16 -0400

    updated modal.py docstring

--- openadapt/utils/modal.py
+++ openadapt/utils/modal.py
@@ -3,7 +3,6 @@ import modal
 BASE_PACKAGE_LIST = [
     "accelerate==0.18.0",
     "fire==0.5.0",
-    "hydra-core==1.2.0",
     "numpy==1.21.2",
     "openai==0.27.2",
     "nltk==3.6.2",
@@ -18,6 +17,14 @@ def create_conda_image(package: str, packages: list[str]):
     """
     Enables the user to create a conda image
     with packages of their choosing.
+
+    This is useful because Modal is being used across various
+    tasks in the repository for inference and fine-tuning when
+    these are not possible on the user's local machine.
+
+    The image has recommended ML packages installed inside
+    BASE_PACKAGE_LIST (such as transformers and torch) and
+    the purpose of this function is to add more, should the user see fit.
     """
     assert package or packages, "Please provide a package, or a list of packages"
     assert not (package and packages), "Please provide only one of: package or packages"
