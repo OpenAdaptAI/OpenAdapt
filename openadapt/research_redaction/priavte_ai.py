c45a4f7692ea246577455829aa31a99c2cc49316
Krish Patel <kmppatel133@gmail.com>
2023-08-28 16:04:58 -0400

    private_ai pdf redaction works

--- openadapt/research_redaction/priavte_ai.py
+++ openadapt/research_redaction/priavte_ai.py
@@ -1,32 +1,35 @@
 import base64
 import os
 
-from loguru import logger
-from privateai_client import PAIClient
-from privateai_client.objects import request_objects
+import requests
 
-from openadapt import config
+url = "https://api.private-ai.com/deid/v3/process/files/base64"
 
-file_dir = "files"
+file_dir = "files/"
 file_name = "sample_llc_1.pdf"
 filepath = os.path.join(file_dir, file_name)
-file_type = "application/pdf"  # eg. application/pdf
-client = PAIClient(url="http://localhost:8080")
+file_type = "application/pdf"
 
 # Read from file
 with open(filepath, "rb") as b64_file:
     file_data = base64.b64encode(b64_file.read())
     file_data = file_data.decode("ascii")
 
-# Make the request
-file_obj = request_objects.file_obj(data=file_data, content_type=file_type)
-request_obj = request_objects.file_base64_obj(file=file_obj)
-resp = client.process_files_base64(request_object=request_obj)
-if not resp.ok:
-    logger.warning(f"response for file {file_name} returned with {resp.status_code}")
+
+payload = {
+    "file": {"data": file_data, "content_type": file_type},
+    "entity_detection": {"accuracy": "high", "return_entity": True},
+    "pdf_options": {"density": 150, "max_resolution": 2000},
+    "audio_options": {"bleep_start_padding": 0, "bleep_end_padding": 0},
+}
+
+headers = {"Content-Type": "application/json", "X-API-KEY": "MRSLHS7HUX4G"}
+
+response = requests.post(url, json=payload, headers=headers)
+response = response.json()
 
 # Write to file
 with open(os.path.join(file_dir, f"redacted-{file_name}"), "wb") as redacted_file:
-    processed_file = resp.processed_file.encode("ascii")
+    processed_file = response.get("processed_file").encode("ascii")
     processed_file = base64.b64decode(processed_file, validate=True)
     redacted_file.write(processed_file)
