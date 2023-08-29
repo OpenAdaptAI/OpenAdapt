c99fcff0d9860d7abe2ae2ccfdf4cb1cc8987195
Krish Patel <kmppatel133@gmail.com>
2023-08-29 13:09:51 -0400

    add class `PrivateAIScrubbingProvider`

--- openadapt/privacy/providers/private_ai.py
+++ openadapt/privacy/providers/private_ai.py
@@ -1,1 +1,19 @@
 """A Module for Private AI Scrubbing Provider."""
+
+
+from typing import List
+
+from loguru import logger
+
+from openadapt import config
+from openadapt.privacy.base import Modality, ScrubbingProvider, TextScrubbingMixin
+from openadapt.privacy.providers import ScrubProvider
+
+
+class PrivateAIScrubbingProvider(
+    ScrubProvider, ScrubbingProvider, TextScrubbingMixin
+):  # pylint: disable=abstract-method
+    """A Class for Private AI Scrubbing Provider."""
+
+    name: str = ScrubProvider.COMPREHEND
+    capabilities: List[Modality] = [Modality.TEXT, Modality.PIL_IMAGE, Modality.PDF]
