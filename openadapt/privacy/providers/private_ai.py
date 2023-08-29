dccc0f586699afdddf7df4f4bf2b2613a26c67c3
Krish Patel <kmppatel133@gmail.com>
2023-08-29 13:16:19 -0400

    ran `pre-commit run --all`

--- openadapt/privacy/providers/private_ai.py
+++ openadapt/privacy/providers/private_ai.py
@@ -1,9 +1,9 @@
 """A Module for Private AI Scrubbing Provider."""
 
-
 from typing import List
 
 from loguru import logger
+import requests
 
 from openadapt import config
 from openadapt.privacy.base import Modality, ScrubbingProvider, TextScrubbingMixin
@@ -17,3 +17,14 @@ class PrivateAIScrubbingProvider(
 
     name: str = ScrubProvider.COMPREHEND
     capabilities: List[Modality] = [Modality.TEXT, Modality.PIL_IMAGE, Modality.PDF]
+
+    def scrub_text(self, text: str, is_separated: bool = False) -> str:
+        """Scrub the text of all PII/PHI.
+
+        Args:
+            text (str): Text to be scrubbed
+            is_separated (bool): Whether the text is separated with special characters
+
+        Returns:
+            str: Scrubbed text
+        """
