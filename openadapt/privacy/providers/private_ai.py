a677eff2ce7aecb522e6567162894a0baf26cc9e
Krish Patel <kmppatel133@gmail.com>
2023-08-29 18:56:11 -0400

    progress for image redaction

--- openadapt/privacy/providers/private_ai.py
+++ openadapt/privacy/providers/private_ai.py
@@ -1,6 +1,8 @@
 """A Module for Private AI Scrubbing Provider."""
 
 from typing import List
+import base64
+import os
 
 from loguru import logger
 import requests
@@ -141,4 +143,37 @@ class PrivateAIScrubbingProvider(
         Returns:
             Image: The scrubbed image with PII and PHI removed.
         """
-        raise NotImplementedError
+        file_dir = "files/"
+        file_name = "sample_emr_1.png"
+        filepath = os.path.join(file_dir, file_name)
+        file_type = "image/png"
+
+        # Read from file
+        with open(filepath, "rb") as b64_file:
+            file_data = base64.b64encode(b64_file.read())
+            file_data = file_data.decode("ascii")
+
+        payload = {
+            "file": {"data": file_data, "content_type": file_type},
+            "entity_detection": {"accuracy": "high", "return_entity": True},
+            "pdf_options": {"density": 150, "max_resolution": 2000},
+            "audio_options": {"bleep_start_padding": 0, "bleep_end_padding": 0},
+        }
+
+        headers = {
+            "Content-Type": "application/json",
+            "X-API-KEY": config.PRIVATE_AI_API_KEY,
+        }
+
+        response = requests.post(url, json=payload, headers=headers)
+        response = response.json()
+
+        # Write to file
+        with open(
+            os.path.join(file_dir, f"redacted-{file_name}"), "wb"
+        ) as redacted_file:
+            processed_file = response.get("processed_file").encode("ascii")
+            processed_file = base64.b64decode(processed_file, validate=True)
+            redacted_file.write(processed_file)
+
+        # return PIL_IMAGE type of redacted image
