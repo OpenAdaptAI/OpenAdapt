46fdf8bba4aff7e08d53d1ed8b76e17c41383f00
FFFiend <owais.zahid@mail.utoronto.ca>
2023-09-01 04:18:21 -0400

    added fixture func

--- tests/openadapt/test_openai_finetune.py
+++ tests/openadapt/test_openai_finetune.py
@@ -1,25 +1,35 @@
 from loguru import logger
 import openai
+import pytest
 
-def test_failure_finetuned_completion():
-    with open("/Users/owaiszahid/Desktop/ff/OpenAdapt/tests/openadapt/assets/recording_fixtures.json","r") as file:
 
+@pytest.fixture()
+def load_recording() -> list[dict[str, str]]:
+    """Loads the Action, Window state pairs."""
+    with open(
+        "/Users/owaiszahid/Desktop/ff/OpenAdapt/tests/openadapt/assets/recording_fixtures.json",
+        "r",
+    ) as file:
         incomplete_recording = file.readlines()
 
     for i in range(len(incomplete_recording)):
         incomplete_recording[i] = eval(incomplete_recording[i])
 
+    return incomplete_recording
+
+
+def test_failure_finetuned_completion(load_recording: list[dict[str, str]]) -> None:
+    """Failure test using OpenAI API call."""
+
     prompt_str = ""
 
-    for dict in incomplete_recording:
+    for dict in load_recording:
         prompt_str += str(dict["prompt"]) + ","
 
-    
     test_ft_comp = openai.Completion.create(
-        model="davinci:ft-openadaptai-2023-08-18-04-09-43", 
-        prompt = prompt_str,
-        max_tokens=388
+        model="davinci:ft-openadaptai-2023-08-18-04-09-43",
+        prompt=prompt_str,
+        max_tokens=388,
     )
 
-    assert (eval(test_ft_comp["choices"]) != incomplete_recording)
-
+    assert eval(test_ft_comp["choices"]) != load_recording
