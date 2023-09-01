b008fc94f32e8d1ccc6d84d8db745054d51d3065
FFFiend <owais.zahid@mail.utoronto.ca>
2023-09-01 00:43:30 -0400

    removed exception

--- openadapt/ml/exceptions.py
+++ openadapt/ml/exceptions.py
@@ -1,13 +1,5 @@
 """Exceptions file for the Completions Provider API."""
 
 
-class ModelNotImplementedError(BaseException):
-    """To be raised when a model is not supported by the infrastructure."""
-
-    def __init__(self, model_name: str) -> None:
-        """Init method for the ModelNotImplementedError class."""
-        super().__init__(f"{model_name} is not currently supported")
-
-
 class GPUNotAvailableError(BaseException):
     """Raised when no GPU is available on the system."""
