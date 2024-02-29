e2f126805585536b48e53ec1e2e368d3ff0d125a
Richard Abrich <richard.abrich@gmail.com>
2024-02-28 23:12:28 -0500

    add pm4py example

---
+++ experiments/pm4py/make_csv.sql
@@ -0,0 +1,11 @@
+select r.id as case_id, 
+	we.title as activity, 
+    -- ae."timestamp" as timestamp,	
+    datetime(ae."timestamp", 'unixepoch', 'localtime') AS "timestamp",
+	COALESCE(ae."timestamp" - LAG(ae."timestamp") OVER (ORDER BY ae."timestamp"), 0) as costs,
+	ae.name	as resource
+from recording r
+inner join action_event ae on r."timestamp" = ae.recording_timestamp 
+inner join window_event we on r."timestamp" = we.recording_timestamp and we."timestamp" = ae.window_event_timestamp
+where r.id = 1
+order by r.id, ae."timestamp";
