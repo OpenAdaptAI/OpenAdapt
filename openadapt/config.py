5769093d2143b6390dc377e55d3a61113da56fa2
Shohan Dutta Roy <shohanduttaroy99@gmail.com>
2024-05-23 18:16:07 +0530

    feat: Alert user when any exception occurs, via sentry hooks

--- openadapt/config.py
+++ openadapt/config.py
@@ -8,11 +8,13 @@ import multiprocessing
 import os
 import pathlib
 import shutil
+import webbrowser
 
 from loguru import logger
 from pydantic import field_validator
 from pydantic.fields import FieldInfo
 from pydantic_settings import BaseSettings, PydanticBaseSettingsSource
+from PySide6.QtWidgets import QMessageBox, QPushButton
 import git
 import sentry_sdk
 
@@ -124,7 +126,7 @@ class Config(BaseSettings):
 
     # Error reporting
     ERROR_REPORTING_ENABLED: bool = True
-    ERROR_REPORTING_DSN: str = (
+    ERROR_REPORTING_DSN: ClassVar = (
         "https://dcf5d7889a3b4b47ae12a3af9ffcbeb7@app.glitchtip.com/3798"
     )
     ERROR_REPORTING_BRANCH: str = "main"
@@ -409,7 +411,34 @@ def print_config() -> None:
                 )
                 logger.info(f"{is_reporting_branch=}")
             if is_reporting_branch:
+
+                def show_alert() -> None:
+                    """Show an alert to the user."""
+                    msg = QMessageBox()
+                    msg.setIcon(QMessageBox.Warning)
+                    msg.setText("""
+                        An error has occurred. The development team has been notified.
+                        Please join the discord server to get help or send an email to
+                        help@openadapt.ai
+                        """)
+                    discord_button = QPushButton("Join the discord server")
+                    discord_button.clicked.connect(
+                        lambda: webbrowser.open("https://discord.gg/yF527cQbDG")
+                    )
+                    msg.addButton(discord_button, QMessageBox.ActionRole)
+                    msg.addButton(QMessageBox.Ok)
+                    msg.exec()
+
+                def before_send_event(event: Any, hint: Any) -> Any:
+                    """Handle the event before sending it to Sentry."""
+                    try:
+                        show_alert()
+                    except Exception:
+                        pass
+                    return event
+
                 sentry_sdk.init(
                     dsn=config.ERROR_REPORTING_DSN,
                     traces_sample_rate=1.0,
+                    before_send=before_send_event,
                 )
