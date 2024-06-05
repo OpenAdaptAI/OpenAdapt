d59dc50e8e0637f317c155239134ae9db009a5cf
Shohan Dutta Roy <shohanduttaroy99@gmail.com>
2024-06-05 11:22:10 +0530

    fix: Fix database revision

--- openadapt/alembic/versions/a29b537fabe6_add_disabled_field_to_action_event.py
+++ openadapt/alembic/versions/a29b537fabe6_add_disabled_field_to_action_event.py
@@ -1,7 +1,7 @@
 """add_disabled_field_to_action_event
 
 Revision ID: a29b537fabe6
-Revises: d714cc86fce8
+Revises: 98c8851a5321
 Create Date: 2024-05-28 11:28:50.353928
 
 """
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "a29b537fabe6"
-down_revision = "d714cc86fce8"
+down_revision = "98c8851a5321"
 branch_labels = None
 depends_on = None
 
