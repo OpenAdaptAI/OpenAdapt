db60b5af298562d577533a126226c56a6738e9d0
Shohan Dutta Roy <shohanduttaroy99@gmail.com>
2024-06-05 11:42:28 +0530

    feat: Add scripts to run the app after build

--- openadapt/entrypoint.py
+++ openadapt/entrypoint.py
@@ -7,17 +7,21 @@ if __name__ == "__main__":
     multiprocessing.freeze_support()
 
 
-from openadapt.alembic.context_loader import load_alembic_context
-from openadapt.app import tray
 from openadapt.build_utils import redirect_stdout_stderr
-from openadapt.config import print_config
 
 
 def run_openadapt() -> None:
     """Run OpenAdapt."""
-    print_config()
-    load_alembic_context()
-    tray._run()
+    try:
+        from openadapt.alembic.context_loader import load_alembic_context
+        from openadapt.app import tray
+        from openadapt.config import print_config
+
+        print_config()
+        load_alembic_context()
+        tray._run()
+    except Exception:
+        pass
 
 
 if __name__ == "__main__":
