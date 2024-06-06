8da2039d3bbfaeaae790ee4977c3376eaed630ca
Richard Abrich <richard.abrich@gmail.com>
2024-06-06 11:45:36 -0400

    implemented

---
+++ openadapt/strategies/segment.py
@@ -0,0 +1,256 @@
+"""Extends vanilla strategy to use segmentation to convert description -> coordinate.
+
+Uses FastSAM for segmentation.
+"""
+
+from pprint import pformat
+
+from loguru import logger
+
+from openadapt import adapters, common, models, strategies, utils
+from openadapt.strategies.visual import (
+    add_active_segment_descriptions,
+    get_window_segmentation,
+)
+
+
+PROCESS_EVENTS = True
+INCLUDE_WINDOW = False
+INCLUDE_WINDOW_DATA = False
+
+
+class SegmentReplayStrategy(strategies.base.BaseReplayStrategy):
+    """Segment replay strategy that performs segmentation in addition to vanilla."""
+
+    def __init__(
+        self,
+        recording: models.Recording,
+        replay_instructions: str = "",
+        process_events: bool = PROCESS_EVENTS,
+    ) -> None:
+        """Initialize the SegmentReplayStrategy.
+
+        Args:
+            recording (models.Recording): The recording object.
+            replay_instructions (str): Natural language instructions
+                for how recording should be replayed.
+            process_events (bool): Flag indicating whether to process the events.
+              Defaults to True.
+        """
+        super().__init__(recording)
+        self.replay_instructions = replay_instructions
+        self.process_events = process_events
+        self.action_history = []
+        self.action_event_idx = 0
+
+        add_active_segment_descriptions(recording.processed_action_events)
+        self.recording_description = describe_recording(
+            self.recording,
+            self.process_events,
+        )
+
+    def get_next_action_event(
+        self,
+        screenshot: models.Screenshot,
+        window_event: models.WindowEvent,
+        include_active_window: bool = INCLUDE_WINDOW,
+        include_active_window_data: bool = INCLUDE_WINDOW_DATA,
+    ) -> models.ActionEvent | None:
+        """Get the next ActionEvent for replay.
+
+        Args:
+            screenshot (models.Screenshot): The screenshot object.
+            window_event (models.WindowEvent): The window event object.
+            include_active_window (bool): Whether to include window metadata.
+            include_active_window_data (bool): Whether to retain window a11y data.
+
+        Returns:
+            models.ActionEvent or None: The next ActionEvent for replay or None
+              if there are no more events.
+        """
+        if self.process_events:
+            action_events = self.recording.processed_action_events
+        else:
+            action_events = self.recording.action_events
+        self.action_event_idx += 1
+        num_action_events = len(action_events)
+        if self.action_event_idx >= num_action_events:
+            raise StopIteration()
+        logger.debug(f"{self.action_event_idx=} of {num_action_events=}")
+
+        generated_action_event = generate_action_event(
+            screenshot,
+            window_event,
+            action_events,
+            self.action_history,
+            self.replay_instructions,
+            include_active_window,
+            include_active_window_data,
+        )
+        if not generated_action_event:
+            raise StopIteration()
+
+        # convert segment -> coordinate
+        # (based on visual.py)
+        active_window = models.WindowEvent.get_active_window_event(
+            include_active_window_data
+        )
+
+        active_screenshot = models.Screenshot.take_screenshot()
+        logger.info(f"{active_window=}")
+
+        if generated_action_event.name in common.MOUSE_EVENTS:
+            generated_action_event.screenshot = active_screenshot
+            generated_action_event.window_event = active_window
+            generated_action_event.recording = self.recording
+            exceptions = []
+            while True:
+                active_window_segmentation = get_window_segmentation(
+                    generated_action_event,
+                    exceptions=exceptions,
+                )
+                try:
+                    target_segment_idx = active_window_segmentation.descriptions.index(
+                        generated_action_event.active_segment_description
+                    )
+                except ValueError as exc:
+                    logger.warning(f"{exc=}")
+                    exceptions.append(exc)
+                else:
+                    break
+            target_centroid = active_window_segmentation.centroids[target_segment_idx]
+            # <image space position> = scale_ratio * <window/action space position>
+            width_ratio, height_ratio = utils.get_scale_ratios(
+                generated_action_event
+            )
+            target_mouse_x = target_centroid[0] / width_ratio + active_window.left
+            target_mouse_y = target_centroid[1] / height_ratio + active_window.top
+            generated_action_event.mouse_x = target_mouse_x
+            generated_action_event.mouse_y = target_mouse_y
+
+        self.action_history.append(generated_action_event)
+        return generated_action_event
+
+    def __del__(self) -> None:
+        """Log the action history."""
+        action_history_dicts = [
+            action.to_prompt_dict() for action in self.action_history
+        ]
+        logger.info(f"action_history=\n{pformat(action_history_dicts)}")
+
+
+def describe_recording(
+    recording: models.Recording,
+    process_events: bool,
+    include_window: bool = INCLUDE_WINDOW,
+    include_window_data: bool = INCLUDE_WINDOW_DATA,
+) -> str:
+    """Generate a natural language description of the actions in the recording.
+
+    Given the recorded states, describe what happened.
+
+    Args:
+        recording (models.Recording): the recording to describe.
+        process_events (bool): flag indicating whether to process the events.
+        include_window (bool): flag indicating whether to include window metadata.
+        include_window_data (bool): flag indicating whether to include accessibility
+            API data in each window event.
+
+    Returns:
+        (str) natural language description of the what happened in the recording.
+    """
+    if process_events:
+        action_events = recording.processed_action_events
+    else:
+        action_events = recording.action_events
+    action_dicts = [action.to_prompt_dict() for action in action_events]
+    window_dicts = [
+        action.window_event.to_prompt_dict(include_window_data)
+        for action in action_events
+    ]
+    action_window_dicts = [
+        {
+            "action": action_dict,
+            "window": window_dict if include_window else {},
+        }
+        for action_dict, window_dict in zip(action_dicts, window_dicts)
+    ]
+    images = [action.screenshot.image for action in action_events]
+    system_prompt = utils.render_template_from_file(
+        "prompts/system.j2",
+    )
+    prompt = utils.render_template_from_file(
+        "prompts/describe_recording.j2",
+        action_windows=action_window_dicts,
+    )
+    prompt_adapter = adapters.get_default_prompt_adapter()
+    recording_description = prompt_adapter.prompt(
+        prompt,
+        system_prompt,
+        images,
+    )
+    return recording_description
+
+
+def generate_action_event(
+    current_screenshot: models.Screenshot,
+    current_window_event: models.WindowEvent,
+    recorded_actions: list[models.ActionEvent],
+    replayed_actions: list[models.ActionEvent],
+    replay_instructions: str,
+    include_active_window: bool,
+    include_active_window_data: bool,
+) -> models.ActionEvent:
+    """Modify the given ActionEvents according to the given replay instructions.
+
+    Given the description of what happened, proposed modifications in natural language
+    instructions, the current state, and the actions produced so far, produce the next
+    action.
+
+    Args:
+        current_screenshot (models.Screenshot): current state screenshot
+        current_window_event (models.WindowEvent): current state window data
+        recorded_actions (list[models.ActionEvent]): list of action events from the
+            recording
+        replayed_actions (list[models.ActionEvent]): list of actions produced during
+            current replay
+        replay_instructions (str): proposed modifications in natural language
+            instructions
+        include_active_window (bool): Whether to include active window metadata.
+        include_active_window_data (bool): Whether to retain window a11y data.
+
+    Returns:
+        (models.ActionEvent) the next action event to be played, produced by the model
+    """
+    current_image = current_screenshot.image
+    current_window_dict = current_window_event.to_prompt_dict(
+        include_active_window_data,
+    )
+    recorded_action_dicts = [action.to_prompt_dict() for action in recorded_actions]
+    replayed_action_dicts = [action.to_prompt_dict() for action in replayed_actions]
+
+    system_prompt = utils.render_template_from_file(
+        "prompts/system.j2",
+    )
+    prompt = utils.render_template_from_file(
+        "prompts/generate_action_event--segment.j2",
+        current_window=current_window_dict,
+        recorded_actions=recorded_action_dicts,
+        replayed_actions=replayed_action_dicts,
+        replay_instructions=replay_instructions,
+        include_active_window=include_active_window,
+    )
+    prompt_adapter = adapters.get_default_prompt_adapter()
+    content = prompt_adapter.prompt(
+        prompt,
+        system_prompt,
+        [current_image],
+    )
+    action_dict = utils.parse_code_snippet(content)
+    logger.info(f"{action_dict=}")
+    if not action_dict:
+        # allow early stopping
+        return None
+    action = models.ActionEvent.from_dict(action_dict)
+    logger.info(f"{action=}")
+    return action
