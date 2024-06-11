11630209277d1d9f47e8167cbe66bf255e45cc4e
Richard Abrich <richard.abrich@gmail.com>
2024-06-11 13:54:29 -0400

    install_dashboard from dashboard_dir

--- scripts/postinstall.py
+++ scripts/postinstall.py
@@ -21,18 +21,31 @@ def install_detectron2() -> None:
 
 def install_dashboard() -> None:
     """Install dashboard dependencies based on the operating system."""
-    if sys.platform.startswith("win"):
-        # For Windows, use PowerShell script
-        subprocess.check_call([
-            "powershell",
-            os.path.join("openadapt", "app", "dashboard", "entrypoint.ps1"),
-        ])
-    else:
-        # For Unix-like systems, use Bash script
-        subprocess.check_call([
-            "bash",
-            os.path.join("openadapt", "app", "dashboard", "entrypoint.sh"),
-        ])
+    # Save the original directory to revert back after operations
+    original_directory = os.getcwd()
+    print(f"{original_directory=}")
+
+    # Path to the dashboard directory containing package.json
+    dashboard_dir = os.path.join("openadapt", "app", "dashboard")
+    print(f"{dashboard_dir=}")
+
+    # Ensure the dashboard directory exists and contains package.json
+    if not os.path.exists(os.path.join(dashboard_dir, "package.json")):
+        raise FileNotFoundError("package.json not found in the dashboard directory.")
+
+    try:
+        # Change the current working directory to the dashboard directory
+        os.chdir(dashboard_dir)
+
+        if sys.platform.startswith("win"):
+            # For Windows, use PowerShell script
+            subprocess.check_call(["powershell", "entrypoint.ps1"])
+        else:
+            # For Unix-like systems, use Bash script
+            subprocess.check_call(["bash", "entrypoint.sh"])
+    finally:
+        # Revert back to the original directory regardless of success or failure
+        os.chdir(original_directory)
 
 
 def main() -> None:
