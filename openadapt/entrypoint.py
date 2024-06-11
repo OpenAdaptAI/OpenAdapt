4d11b59f8f73f948c59dac398e479a25f2825b04
Shohan Dutta Roy <shohanduttaroy99@gmail.com>
2024-06-11 12:20:35 +0530

    feat: Log error in a file

--- openadapt/entrypoint.py
+++ openadapt/entrypoint.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     # This needs to be called before any code that uses multiprocessing
     multiprocessing.freeze_support()
 
+from datetime import datetime
 
-from openadapt.build_utils import redirect_stdout_stderr
+from openadapt.build_utils import get_root_dir_path, redirect_stdout_stderr
 
 
 def run_openadapt() -> None:
@@ -20,8 +21,10 @@ def run_openadapt() -> None:
         print_config()
         load_alembic_context()
         tray._run()
-    except Exception:
-        pass
+    except Exception as exc:
+        data_dir = get_root_dir_path()
+        with open(data_dir / "error.log", "a") as f:
+            f.write(f"{datetime.now()}: {exc}\n")
 
 
 if __name__ == "__main__":
