22b6b77a31c9c77cc06890f428ee1146d4a88590
Richard Abrich <richard.abrich@gmail.com>
2024-06-12 09:58:42 -0400

    black

--- tests/openadapt/test_utils.py
+++ tests/openadapt/test_utils.py
@@ -8,9 +8,9 @@ def test_get_scale_ratios() -> None:
     # TODO: pass in action event
     width, height = utils.get_scale_ratios()
 
-    assert isinstance(width, (int, float)), (
-        f"Expected width to be int or float, got {type(width).__name__}"
-    )
-    assert isinstance(height, (int, float)), (
-        f"Expected height to be int or float, got {type(height).__name__}"
-    )
+    assert isinstance(
+        width, (int, float)
+    ), f"Expected width to be int or float, got {type(width).__name__}"
+    assert isinstance(
+        height, (int, float)
+    ), f"Expected height to be int or float, got {type(height).__name__}"
