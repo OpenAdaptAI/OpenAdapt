fdd140edfe2fa9702ccaa729b7db87a5040aeaf5
shashank40 <shashanktiwari40@gmail.com>
2024-07-05 00:23:44 +0530

    Added try catch to set permissions

--- openadapt/update_utils.py
+++ openadapt/update_utils.py
@@ -8,9 +8,21 @@ def set_permissions(path: str) -> None:
     """Set the permissions of all files to make the executable."""
     for root, dirs, files in os.walk(path):
         for dir in dirs:
-            os.chmod(os.path.join(root, dir), 0o755)
+            dir_path = os.path.join(root, dir)
+            try:
+                os.chmod(os.path.join(root, dir), 0o755)
+            except PermissionError:
+                print(f"Skipping directory due to PermissionError: {dir_path}")
+            except Exception as e:
+                print(f"An error occurred for directory {dir_path}: {e}")
         for file in files:
-            os.chmod(os.path.join(root, file), 0o755)
+            file_path = os.path.join(root, file)
+            try:
+                os.chmod(os.path.join(root, file), 0o755)
+            except PermissionError:
+                print(f"Skipping file due to PermissionError: {file_path}")
+            except Exception as e:
+                print(f"An error occurred for file {file_path}: {e}")
 
 
 def unzip_file(file_path: str) -> None:
