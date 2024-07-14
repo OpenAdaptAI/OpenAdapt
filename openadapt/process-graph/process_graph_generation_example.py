d354557cc11d3385a9aef37b51d65db2bd45a09f
pate1595 <kmp.patel@utoronto.ca>
2024-07-14 13:30:54 -0400

    wip: todo: not able to view bpmn model

--- openadapt/process-graph/process_graph_generation_example.py
+++ openadapt/process-graph/process_graph_generation_example.py
@@ -1,12 +1,9 @@
+import pm4py
 import pandas
 
-
-def import_csv(file_path):
-    event_log = pandas.read_csv(file_path, sep=';')
-    num_events = len(event_log)
-    num_cases = len(event_log.case_id.unique())
-    print("Number of events: {}\nNumber of cases: {}".format(num_events, num_cases))
-
-
 if __name__ == "__main__":
-    import_csv("running-example.csv")
+    log = pandas.read_csv('running-example.csv', sep=';')
+    log = pm4py.format_dataframe(log, case_id='case_id', activity_key='activity', timestamp_key='timestamp')
+
+    dfg, start_activities, end_activities = pm4py.discover_dfg(log)
+    pm4py.view_dfg(dfg, start_activities, end_activities)
