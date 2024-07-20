8c3be28ba5c48254bb8f2dcf51cf975026c63fcd
pate1595 <kmp.patel@utoronto.ca>
2024-07-20 15:08:02 -0400

    wip process-graph

--- openadapt/process-graph/process_graph_generation_example.py
+++ openadapt/process-graph/process_graph_generation_example.py
@@ -2,8 +2,14 @@ import pm4py
 import pandas
 
 if __name__ == "__main__":
-    log = pandas.read_csv('running-example.csv', sep=';')
-    log = pm4py.format_dataframe(log, case_id='case_id', activity_key='activity', timestamp_key='timestamp')
+    log = pandas.read_csv("dataout.csv", sep=",")
+    log = pm4py.format_dataframe(
+        log,
+        case_id="case_id",
+        activity_key="activity",
+        timestamp_key="timestamp",
+        timest_format="%Y-%m-%d %H:%M:%S",
+    )
 
     dfg, start_activities, end_activities = pm4py.discover_dfg(log)
-    pm4py.view_dfg(dfg, start_activities, end_activities, format='html')
+    pm4py.view_dfg(dfg, start_activities, end_activities, format="html")
