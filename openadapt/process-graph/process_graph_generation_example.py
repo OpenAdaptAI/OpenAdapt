d7a2e6c441fa28944f52ef01a4f1b00e1d5815fc
pate1595 <kmp.patel@utoronto.ca>
2024-07-20 14:26:43 -0400

    working example and outputs the process graph in html

--- openadapt/process-graph/process_graph_generation_example.py
+++ openadapt/process-graph/process_graph_generation_example.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     log = pm4py.format_dataframe(log, case_id='case_id', activity_key='activity', timestamp_key='timestamp')
 
     dfg, start_activities, end_activities = pm4py.discover_dfg(log)
-    pm4py.view_dfg(dfg, start_activities, end_activities)
+    pm4py.view_dfg(dfg, start_activities, end_activities, format='html')
